6b0a3fd78e2be43ad73ef9380d30a8b26a2a0961
Ufuk <ufukty@gmail.com>
2021-08-15 22:37:53 +0300

    floating corners

--- src/App.js
+++ src/App.js
@@ -5,10 +5,31 @@ import "./App.css";
 function App() {
     return (
         <div className="document-sheet">
-            {/* // <HomeButton />
-            // <DocumentSettings />
-            // <ActiveTaskDetails />
-            // <DatePicker /> */}
+            <a
+                id="home-button"
+                class="floating-corner left top"
+                href="index.html"
+            >
+                Logbook
+            </a>
+
+            <div id="sheet-settings" class="floating-corner right top dark">
+                <div>Share</div>
+
+                <div>Sync</div>
+            </div>
+
+            <div id="active-task-details" class="floating-corner left bottom">
+                History for active task
+                <div class="task">PAM for SSH</div>
+                <div class="task">ACL - Redis</div>
+                <div class="task">TOTP for SSH</div>
+            </div>
+
+            <div id="date-anchors" class="floating-corner right bottom">
+                <a href="#august-13-2021">13th August</a>
+                <a href="#august-14-2021">14th August</a>
+            </div>
             <TaskDay />
         </div>
     );
