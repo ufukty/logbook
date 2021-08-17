67a64e1dfe3c7cf9f4f7868036da4581f7c73ef4
Ufuk <ufukty@gmail.com>
2021-08-17 11:21:41 +0300

    tree view populated with recursive component creation

--- src/App.js
+++ src/App.js
@@ -268,6 +268,240 @@ var dataset = {
     ],
 };
 
+var dataset_tree = [
+    {
+        text: "Revoke passwordless sudo rights after provision at cluster",
+        id: 0,
+        parent: -1,
+        created_at: "445884458844588",
+        active: true,
+    },
+    {
+        text: "iptables for redis",
+        id: 2,
+        parent: 0,
+        created_at: "425042504250",
+        active: true,
+    },
+    {
+        text: "terraform for redis",
+        id: 3,
+        parent: 2,
+        created_at: "391839183918",
+        active: true,
+    },
+    {
+        text: "Update redis/tf file according to prod.tfvars file",
+        id: 5,
+        parent: 2,
+        created_at: "227322273222732",
+        active: true,
+    },
+    {
+        text: "gitlab-runner --(vpn)--> DNS ----> gitlab",
+        id: 13,
+        parent: 6,
+        created_at: "996499649964",
+        active: true,
+    },
+    {
+        text: "Firewall & unbound rules update from prov script (VPN)",
+        id: 14,
+        parent: 2,
+        created_at: "649286492864928",
+        active: true,
+    },
+    {
+        text: "Script pic_gitlab_runner_post_creation",
+        id: 15,
+        parent: 3,
+        created_at: "475904759047590",
+        active: true,
+    },
+    {
+        text: "Execute 1 CI/CD pipeline on gitlab-runner",
+        id: 16,
+        parent: 4,
+        created_at: "450134501345013",
+        active: true,
+    },
+    {
+        text: "gitlab-runner provisioner with resolv.conf/docker/runner-register",
+        id: 17,
+        parent: 1,
+        created_at: "339853398533985",
+        active: true,
+    },
+    {
+        text: "Remove: seperator from ovpn-auth",
+        id: 18,
+        parent: 2,
+        created_at: "475904759047590",
+        active: true,
+    },
+    {
+        text: "Write tests for ovpn-auth",
+        id: 19,
+        parent: 1,
+        created_at: "450134501345013",
+        active: true,
+    },
+    {
+        text: "Decrease timing gap of ovpn-auth under 1ms",
+        id: 20,
+        parent: 0,
+        created_at: "339853398533985",
+        active: true,
+    },
+    {
+        text: "Prepare releases for ovpn-auth",
+        id: 21,
+        parent: 3,
+        created_at: "339853398533985",
+        active: true,
+    },
+    {
+        text: "Provision golden-image for gitlab-runner",
+        id: 22,
+        parent: 5,
+        created_at: "339853398533985",
+        active: true,
+    },
+    {
+        text: "prepare gitlab-ci for ovpn-auth repo",
+        id: 23,
+        parent: 5,
+        created_at: "339853398533985",
+        active: false,
+    },
+    {
+        text: "PAM for SSH",
+        id: 24,
+        parent: 3,
+        created_at: "337543375433754",
+        active: true,
+    },
+    {
+        text: "ACL - Redis",
+        id: 25,
+        parent: 7,
+        created_at: "324363243632436",
+        active: true,
+    },
+    {
+        text: "Redis security",
+        id: 26,
+        parent: 2,
+        created_at: "334063340633406",
+        active: false,
+    },
+    {
+        text: "TOTP for SSH",
+        id: 27,
+        parent: 3,
+        created_at: "880588058805",
+        active: false,
+    },
+    {
+        text: "API gateway without redis",
+        id: 28,
+        parent: 0,
+        created_at: "582358235823",
+        active: false,
+    },
+    {
+        text: "Golden image interitance re-organize",
+        id: 29,
+        parent: 3,
+        created_at: "360893608936089",
+        active: false,
+    },
+    {
+        text: "Postgres",
+        id: 30,
+        parent: 3,
+        created_at: "607006070060700",
+        active: false,
+    },
+    {
+        text: "Auth service",
+        id: 31,
+        parent: 4,
+        created_at: "359643596435964",
+        active: false,
+    },
+    {
+        text: "MQ",
+        id: 32,
+        parent: 2,
+        created_at: "996499649964",
+        active: false,
+    },
+    {
+        text: "Federated learning",
+        id: 33,
+        parent: 3,
+        created_at: "649286492864928",
+        active: false,
+    },
+    {
+        text: "Bluetooth transmission test",
+        id: 34,
+        parent: 6,
+        created_at: "475904759047590",
+        active: false,
+    },
+    {
+        text: "Intrusion detection system (centralised) (OSSEC",
+        id: 35,
+        parent: 28,
+        created_at: "450134501345013",
+        active: false,
+    },
+    {
+        text: "Envoy - HAProxy - NGiNX",
+        id: 36,
+        parent: 7,
+        created_at: "339853398533985",
+        active: false,
+    },
+    {
+        text: "web-front/Privacy against [friend/pubic/company/attackers]",
+        id: 37,
+        parent: 7,
+        created_at: "996499649964",
+        active: false,
+    },
+    {
+        text: "Redis/cluster script test for multi datacenter",
+        id: 38,
+        parent: 7,
+        created_at: "649286492864928",
+        active: false,
+    },
+    {
+        text: "gitlab-runner firewall rules: close public internet",
+        id: 39,
+        parent: 5,
+        created_at: "339853398533985",
+        active: false,
+    },
+    {
+        text: "static-challange for ovpn-auth",
+        id: 40,
+        parent: 6,
+        created_at: "339853398533985",
+        active: false,
+    },
+    {
+        text: "Golden image for vpn server",
+        id: 41,
+        parent: 3,
+        created_at: "339853398533985",
+        active: false,
+    },
+];
+
 class App extends React.Component {
     constructor() {
         super();
@@ -280,8 +514,8 @@ class App extends React.Component {
         var content;
         if (this.state.mode === "chronological") {
             content = <ChronologicalView dataset={dataset} />;
-        } else if (this.state.mode == "tree") {
-            content = <TreeView />;
+        } else if (this.state.mode === "tree") {
+            content = <TreeView dataset={dataset_tree} />;
         }
         return (
             <div className="document-sheet">
--- src/views/tree-view/TreeView.js
+++ src/views/tree-view/TreeView.js
@@ -1,12 +1,82 @@
 import React from "react";
 
-class TreeView extends React.Component {
-    constructor() {
+import "./TreeView.css";
+
+import Task from "./../../ui-components/task-group/task-list/task/Task";
+
+class IndentationGroup extends React.Component {
+    constructor(props) {
         super();
+        this.state = {
+            root: props.root,
+            dataset: props.dataset,
+            children: props.dataset
+                .filter((child) => child.parent === props.root.id)
+                .map((child) => (
+                    <IndentationGroup dataset={props.dataset} root={child} />
+                )),
+        };
+        console.log(this.state.root.text);
+        console.log(this.state.dataset);
+        console.log(this.state.children.length);
     }
 
     render() {
-        return <div>dsd</div>;
+        return (
+            <div className="task-indentation-group">
+                <details open="true">
+                    <summary>
+                        <Task key={this.state.root.id} task={this.state.root} />
+                    </summary>
+                    <div className="task-indentation-margin">
+                        {this.state.children}
+                    </div>
+                </details>
+            </div>
+        );
+    }
+}
+
+class TreeView extends React.Component {
+    constructor(props) {
+        super();
+        this.state = {
+            dataset: props.dataset,
+        };
+    }
+    render() {
+        return (
+            <div>
+                <IndentationGroup
+                    dataset={this.state.dataset}
+                    root={this.state.dataset[0]}
+                />
+                <IndentationGroup
+                    dataset={this.state.dataset}
+                    root={this.state.dataset[2]}
+                />
+                <IndentationGroup
+                    dataset={this.state.dataset}
+                    root={this.state.dataset[3]}
+                />
+                <IndentationGroup
+                    dataset={this.state.dataset}
+                    root={this.state.dataset[4]}
+                />
+                <IndentationGroup
+                    dataset={this.state.dataset}
+                    root={this.state.dataset[5]}
+                />
+                <IndentationGroup
+                    dataset={this.state.dataset}
+                    root={this.state.dataset[6]}
+                />
+                <IndentationGroup
+                    dataset={this.state.dataset}
+                    root={this.state.dataset[7]}
+                />
+            </div>
+        );
     }
 }
 
