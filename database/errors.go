4c715c889205ece2f71237c40bee755c80a04e3b
Ufuk <ufukty@gmail.com>
2021-08-27 13:15:51 +0300

    database/errors initial

---
+++ database/errors.go
@@ -0,0 +1,76 @@
+package database
+
+import "errors"
+
+var ErrNotSpecified error
+
+var ErrNoResult error
+
+var ErrInvalidInput error
+
+var ErrEmptyUserId error
+
+var ErrEmptyDocumentId error
+
+var ErrEmptyTaskGroupId error
+
+// FIXME: Wrap the PSQL error with Err class
+
+func initErrors() {
+	ErrNotSpecified = errors.New("there is an unexpected error")
+	ErrNoResult = errors.New("no matching record find with those parameters")
+	ErrEmptyUserId = errors.New("user Id required to perform this operation, but none provided")
+	ErrInvalidInput = errors.New("database didn't like the format of input")
+}
+
+func checkUserId(userId string) error {
+	// check existance
+	if userId == "" {
+		return ErrEmptyUserId
+	}
+	// check validity
+	if _, err := GetUserByUserId(userId); err != nil {
+		return err
+	}
+	return nil
+}
+
+func checkDocumentId(documentId string) error {
+	// check existance
+	if documentId == "" {
+		return ErrEmptyDocumentId
+	}
+	// check validity
+	if _, err := GetDocumentByDocumentId(documentId); err != nil {
+		return err
+	}
+	return nil
+}
+
+func checkTaskGroupId(taskGroupId string) error {
+	// check existance
+	if taskGroupId == "" {
+		return ErrEmptyTaskGroupId
+	}
+	// check validity
+	if _, err := GetTaskGroupByTaskGroupId(taskGroupId); err != nil {
+		return err
+	}
+	return nil
+}
+
+func checkTaskId(taskId string) error { // TODO:
+	return nil
+}
+
+func exportError(err error) error {
+	if err == nil {
+		return nil
+	}
+	switch err.Error() {
+	case "no rows in result set":
+		return ErrNoResult
+	default:
+		return ErrNotSpecified
+	}
+}
