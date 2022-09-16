e28cb6e0ced8516903c148ce2893cae4be4f0c88
Ufuk <ufukty@gmail.com>
2022-09-16 23:50:57 +0300

    js/dataSoure new mock dataset generator

--- js/dataSource.js
+++ js/dataSource.js
@@ -20,6 +20,23 @@ export const EVENT_PLACEMENT_UPDATE = "EVENT_PLACEMENT_UPDATE";
  * accept list of objectIds as argument */
 export const EVENT_OBJECT_UPDATE = "EVENT_OBJECT_UPDATE";
 
+function shuffle(array) {
+    let currentIndex = array.length,
+        randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+}
+
 export class DataSource {
     constructor() {
         this.medium = new InfiniteSheetDataMedium();
@@ -131,94 +148,28 @@ export class DataSource {
     }
 
     loadTestDataset() {
-        this.cache.placements.totalNumberOfItems = 1000;
-
-        const symbols = {
-            "task#123": symbolizer.symbolize("task#123"),
-            "task#143": symbolizer.symbolize("task#143"),
-            "task#133": symbolizer.symbolize("task#133"),
-            "task#124": symbolizer.symbolize("task#124"),
-            "task#144": symbolizer.symbolize("task#144"),
-            "task#134": symbolizer.symbolize("task#134"),
-            "task#125": symbolizer.symbolize("task#125"),
-            "task#145": symbolizer.symbolize("task#145"),
-            "task#126": symbolizer.symbolize("task#126"),
-            "task#146": symbolizer.symbolize("task#146"),
-            "task#135": symbolizer.symbolize("task#135"),
-            "task#127": symbolizer.symbolize("task#127"),
-            "task#147": symbolizer.symbolize("task#147"),
-            "task#136": symbolizer.symbolize("task#136"),
-            "task#137": symbolizer.symbolize("task#137"),
-            "day#0": symbolizer.symbolize("day#0"),
-            "day#1": symbolizer.symbolize("day#1"),
-            "day#2": symbolizer.symbolize("day#2"),
-            "day#3": symbolizer.symbolize("day#3"),
-        };
-
-        this.cache.placements.chronological = {
-            headerSymbols: [symbols["day#0"], symbols["day#1"], symbols["day#2"], symbols["day#3"]],
-            offset: 0,
-            items: [
-                symbols["day#0"],
-                symbols["task#123"],
-                symbols["task#124"],
-                symbols["task#125"],
-                symbols["task#126"],
-                symbols["day#1"],
-                symbols["task#127"],
-                symbols["task#133"],
-                symbols["task#134"],
-                symbols["task#135"],
-                symbols["day#2"],
-                symbols["task#136"],
-                symbols["task#137"],
-                symbols["task#143"],
-                symbols["task#144"],
-                symbols["day#3"],
-                symbols["task#145"],
-                symbols["task#146"],
-                symbols["task#147"],
-            ],
-        };
-
-        this.cache.placements.hierarchical = {
-            offset: 0,
-            items: [
-                symbols["task#123"],
-                symbols["task#143"],
-                symbols["task#133"],
-                symbols["task#124"],
-                symbols["task#144"],
-                symbols["task#134"],
-                symbols["task#125"],
-                symbols["task#145"],
-                symbols["task#126"],
-                symbols["task#146"],
-                symbols["task#135"],
-                symbols["task#127"],
-                symbols["task#147"],
-                symbols["task#136"],
-                symbols["task#137"],
-            ],
-        };
-
-        // prettier-ignore
-        this.cache.tasks.set(symbols["task#123"], { content: "task#123", depth: 0, parentId: symbols["-1"] });
-        this.cache.tasks.set(symbols["task#143"], { content: "task#143", depth: 1, parentId: symbols["task#123"] });
-        this.cache.tasks.set(symbols["task#144"], { content: "task#144", depth: 2, parentId: symbols["task#143"] });
-        this.cache.tasks.set(symbols["task#126"], { content: "task#126", depth: 3, parentId: symbols["task#144"] });
-        this.cache.tasks.set(symbols["task#146"], { content: "task#146", depth: 3, parentId: symbols["task#144"] });
-        this.cache.tasks.set(symbols["task#134"], { content: "task#134", depth: 2, parentId: symbols["task#143"] });
-        this.cache.tasks.set(symbols["task#125"], { content: "task#125", depth: 2, parentId: symbols["task#143"] });
-        this.cache.tasks.set(symbols["task#137"], { content: "task#137", depth: 3, parentId: symbols["task#125"] });
-        this.cache.tasks.set(symbols["task#145"], { content: "task#145", depth: 2, parentId: symbols["task#143"] });
-        this.cache.tasks.set(symbols["task#135"], { content: "task#135", depth: 3, parentId: symbols["task#145"] });
-        this.cache.tasks.set(symbols["task#133"], { content: "task#133", depth: 1, parentId: symbols["task#123"] });
-        this.cache.tasks.set(symbols["task#127"], { content: "task#127", depth: 2, parentId: symbols["task#133"] });
-        this.cache.tasks.set(symbols["task#147"], { content: "task#147", depth: 2, parentId: symbols["task#133"] });
-        this.cache.tasks.set(symbols["task#136"], { content: "task#136", depth: 2, parentId: symbols["task#133"] });
-        this.cache.tasks.set(symbols["task#124"], { content: "task#124", depth: 1, parentId: symbols["task#123"] });
-
+        this.cache.placements.totalNumberOfItems = 100;
+        var dayCounter = 1;
+        const symbols = new Map();
+        const taskIndexes = Array.from(Array(100).keys());
+        taskIndexes.forEach((taskIndex, index) => {
+            if (index === 0 || Math.random() > 0.8) {
+                // add header
+                const dayId = "day#" + dayCounter.toString();
+                const daySymbol = symbolizer.symbolize(dayId);
+                symbols.set(dayId, daySymbol);
+                this.cache.placements.chronological.headerSymbols.push(daySymbol);
+                this.cache.placements.chronological.items.push(daySymbol);
+                dayCounter += 1;
+            }
+            const taskId = "task#" + taskIndex.toString();
+            const taskSymbol = symbolizer.symbolize(taskId);
+            symbols.set(taskId, taskSymbol);
+            this.cache.placements.hierarchical.items.push(taskSymbol);
+            this.cache.placements.chronological.items.push(taskSymbol);
+            this.cache.tasks.set(taskSymbol, { content: taskId });
+        });
+        shuffle(this.cache.placements.hierarchical.items);
         this.delegates.nofify(EVENT_PLACEMENT_UPDATE);
     }
 
