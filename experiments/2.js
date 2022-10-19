c67932ee04d6d7197b03f0b1216ce628d60600df
Ufuk <ufukty@gmail.com>
2022-10-19 11:45:57 +0300

    exp/2: route preview component top->translate

--- experiments/2.js
+++ experiments/2.js
@@ -3,6 +3,23 @@ import { clamp, executeWhenDocumentIsReady, inverseLerp, lerp } from "../js/baja
 /** @type {Array.<HTMLElement>} */
 const routeContainers = [...document.getElementsByClassName("route-container")];
 
+const elementUpdater = (routeContainer, carLight, reflectiveLayers, clampedMidPointRouteContainer) => {
+    const relativePosX = routeContainer.offsetWidth / 2;
+    const relativePosY = lerp(0, routeContainer.offsetHeight, clampedMidPointRouteContainer);
+
+    const maskPositionX = `${-routeContainer.offsetWidth / 2 + relativePosX}`;
+    const maskPositionY = `${-routeContainer.offsetHeight / 2 + relativePosY}`;
+
+    carLight.style.transform = `translateY(calc(-50% + ${relativePosY}px))`;
+
+    reflectiveLayers[0].style.webkitMaskPosition = `${maskPositionX}px ${maskPositionY}px`;
+    reflectiveLayers[1].style.webkitMaskPosition = `${maskPositionX}px ${maskPositionY}px`;
+    reflectiveLayers[0].style.maskPosition = `${maskPositionX}px ${maskPositionY}px`;
+    reflectiveLayers[1].style.maskPosition = `${maskPositionX}px ${maskPositionY}px`;
+
+    routeContainer.dataset.clampedMidPointRouteContainer = clampedMidPointRouteContainer;
+};
+
 var scrollPositionForLastUpdate = undefined;
 const adjustReflectionsWithScrollPosition = () => {
     if (scrollPositionForLastUpdate && scrollPositionForLastUpdate === window.scrollY) return;
@@ -23,28 +40,14 @@ const adjustReflectionsWithScrollPosition = () => {
         const clampedMidPointRouteContainer = clamp(midPointRouteContainer, -0.5, 1.5);
 
         if (
-            routeContainer.clampedMidPointRouteContainer &&
-            routeContainer.clampedMidPointRouteContainer === clampedMidPointRouteContainer
-        ) {
+            routeContainer.dataset.clampedMidPointRouteContainer &&
+            routeContainer.dataset.clampedMidPointRouteContainer === clampedMidPointRouteContainer
+        )
             return;
-        }
 
-        const relativePosX = routeContainer.offsetWidth / 2;
-        const relativePosY = lerp(0, routeContainer.offsetHeight, clampedMidPointRouteContainer);
-
-        const maskPositionX = `${-routeContainer.offsetWidth / 2 + relativePosX}`;
-        const maskPositionY = `${-routeContainer.offsetHeight / 2 + relativePosY}`;
-
-        carLight.style.top = `${relativePosY}px`;
-        // carLight.style.left = `${relativePosX}px`;
-
-        reflectiveLayers[0].style.webkitMaskPosition = `${maskPositionX}px ${maskPositionY}px`;
-        reflectiveLayers[1].style.webkitMaskPosition = `${maskPositionX}px ${maskPositionY}px`;
-        reflectiveLayers[0].style.maskPosition = `${maskPositionX}px ${maskPositionY}px`;
-        reflectiveLayers[1].style.maskPosition = `${maskPositionX}px ${maskPositionY}px`;
-
-        // console.log(midPointRouteContainer);
-        routeContainer.clampedMidPointRouteContainer = clampedMidPointRouteContainer;
+        requestAnimationFrame(
+            elementUpdater.bind(undefined, routeContainer, carLight, reflectiveLayers, clampedMidPointRouteContainer)
+        );
     });
 
     scrollPositionForLastUpdate = window.scrollY;
