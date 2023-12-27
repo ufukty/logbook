36b7c94efe075750091500c3a8fb0ef8d363bd9b
Ufuk <13807261+ufukty@users.noreply.github.com>
2023-12-28 00:23:00 +0300

    reworks cmd/task database models for version control

--- app/cmd/tasks/database/models.go
+++ app/cmd/tasks/database/models.go
@@ -1,107 +1,78 @@
 package database
 
 import (
-	"database/sql"
-	"time"
-
 	"github.com/jackc/pgtype"
 )
 
-type OperationSummary string
-
-const (
-	TASK_CREATE               OperationSummary = "TASK_CREATE"
-	TASK_REORDER              OperationSummary = "TASK_REORDER"
-	TASK_DELETE               OperationSummary = "TASK_DELETE"
-	TASK_CONTENT_EDIT         OperationSummary = "TASK_CONTENT_EDIT"
-	TASK_MARK_COMPLETE        OperationSummary = "TASK_MARK_COMPLETE"
-	TASK_MARK_UNCOMPLETE      OperationSummary = "TASK_MARK_UNCOMPLETE"
-	NOTE_CREATE               OperationSummary = "NOTE_CREATE"
-	NOTE_EDIT                 OperationSummary = "NOTE_EDIT"
-	NOTE_DELETE               OperationSummary = "NOTE_DELETE"
-	COLLABORATION_ASSIGN      OperationSummary = "COLLABORATION_ASSIGN"
-	COLLABORATION_UNASSIGN    OperationSummary = "COLLABORATION_UNASSIGN"
-	COLLABORATION_RESTRICT    OperationSummary = "COLLABORATION_RESTRICT"
-	COLLABORATION_DERESTRICT  OperationSummary = "COLLABORATION_DERESTRICT"
-	COLLABORATION_CHANGE_ROLE OperationSummary = "COLLABORATION_CHANGE_ROLE"
-	HISTORY_ROLLBACK          OperationSummary = "HISTORY_ROLLBACK"
-	HISTORY_FASTFORWARD       OperationSummary = "HISTORY_FASTFORWARD"
-)
-
-type OperationStatus string
-
-const (
-	SERVER_ORIGINATED           OperationStatus = "SERVER_ORIGINATED"
-	IN_REVIEW                   OperationStatus = "IN_REVIEW"
-	PRIV_ACCEPTED               OperationStatus = "PRIV_ACCEPTED"
-	PRIV_REJECTED               OperationStatus = "PRIV_REJECTED"
-	APPLIED_FASTFORWARD         OperationStatus = "APPLIED_FASTFORWARD"
-	CONFLICT_DETECTED           OperationStatus = "CONFLICT_DETECTED"
-	MANAGER_SELECTION_IN_REVIEW OperationStatus = "MANAGER_SELECTION_IN_REVIEW"
-	MANAGER_SELECTION_ACCEPTED  OperationStatus = "MANAGER_SELECTION_ACCEPTED"
-	MANAGER_SELECTION_APPLIED   OperationStatus = "MANAGER_SELECTION_APPLIED"
-	MANAGER_SELECTION_REJECTED  OperationStatus = "MANAGER_SELECTION_REJECTED"
-)
-
 type Operation struct {
-	OperationId         string           `gorm:"<-:create;->;unique;default:gen_random_UUID()"`
-	PreviousOperationId sql.NullString   `gorm:"<-:create;->"`
-	UserId              string           `gorm:"<-:create;->;not null"`
-	Summary             OperationSummary `gorm:"<-:create;->;not null"`
-	Status              OperationStatus  `gorm:"<-;->;not null"`
-	CreatedAt           time.Time        `gorm:"<-:create;->;not null;default:CURRENT_TIMESTAMP"`
-	ArchivedAt          time.Time        `gorm:"<-:->"`
+	OpId string `json:"opid"`
+	// PreviousOpId sql.NullString `json:"previous_opid"`
+
+	Vid     VersionId `json:"vid"`
+	PrevVid VersionId `json:"previous_vid"`
+
+	Oid ObjectiveId `json:"oid"`
+
+	UserId string `json:"uid"`
+
+	Summary OperationSummary `json:"summary"`
+	Status  OperationStatus  `json:"status"`
+
+	CreatedAt  pgtype.Date `json:"created_at"`
+	ArchivedAt pgtype.Date `json:"archived_at"`
 }
 
-type Task struct {
-	TaskId        Iid               `json:"task_id"`
-	DocumentId    Did               `json:"document_id"`
-	ParentId      Iid               `json:"parent_id"`
-	Content       string            `json:"content"`
+// objective or goal
+type Objective struct {
+	Oid      ObjectiveId `json:"objective_id"`
+	ParentId ObjectiveId `json:"parent_id"`
+	Content  string      `json:"content"`
+
+	Vid               VersionId   `json:"vid"`
+	Creator           UserId      `json:"creator"`
+	ResponsibleUserId string      `json:"responsible_uid"`
+	ArchivedAt        pgtype.Date `json:"archived_at"`
+	Archived          bool        `json:"archived"`
+	RootObjective     bool        `json:"root_objective"`
+
+	CreatedAt   pgtype.Date `json:"created_at"`
+	CompletedAt pgtype.Date `json:"completed_at"` // nullable type
+}
+
+type ObjectiveLink struct {
+	Lid LinkId    `json:"lid"`
+	Vid VersionId `json:"vid"`
+
+	SupOid ObjectiveId `json:"sup_oid"`
+	SupVid VersionId   `json:"sup_vid"`
+	SubOid ObjectiveId `json:"sub_oid"`
+	SubVid VersionId   `json:"sub_vid"`
+
+	Index     int         `json:"index"`
+	Type      LinkType    `json:"type"`
+	CreatedAt pgtype.Date `json:"created_at"`
+}
+
+// Computed properties and user preferences per item per user
+type ObjectiveView struct {
+	Oid           ObjectiveId
+	Vid           VersionId
+	Uid           UserId
 	Degree        NonNegativeNumber `json:"degree"`
 	Depth         NonNegativeNumber `json:"depth"`
-	CreatedAt     time.Time         `json:"created_at"`
-	CompletedAt   pgtype.Date       `json:"completed_at"` // nullable type
 	ReadyToPickUp bool              `json:"ready_to_pick_up"`
-
-	RevisionId            string    `gorm:"<-:create;->;not null;primaryKey"`
-	TaskId                string    `gorm:"<-:create;->;not null;default:gen_random_UUID()"`
-	OriginalCreatorUserId string    `gorm:"not null"`
-	ResponsibleUserId     string    `gorm:"not null"`
-	Content               string    `gorm:"not null"`
-	CreatedAt             time.Time `gorm:"not null;default:CURRENT_TIMESTAMP"`
-	CompletedAt           time.Time `gorm:""`
-	ArchivedAt            time.Time `gorm:""`
-	Archived              bool      `gorm:""`
-	RootTask              bool      `gorm:"default:FALSE"`
+	Completion    float64           `json:"completion"`
+	Fold          bool              `json:"fold"`
 }
 
-type TaskLink struct {
-	LinkId              string    `gorm:""`
-	RevisionId          string    `gorm:""`
-	TaskId              string    `gorm:""`
-	TaskRevisionId      string    `gorm:""`
-	SuperTaskId         string    `gorm:""`
-	SuperTaskRevisionId string    `gorm:""`
-	Index               string    `gorm:""`
-	PrimaryLink         string    `gorm:""`
-	CreatedAt           time.Time `gorm:"not null;default:CURRENT_TIMESTAMP"`
+type Bookmark struct {
+	UserId       string      `json:"uid"`
+	Oid          ObjectiveId `json:"objective_id"`
+	DisplayName  string      `json:"display_name"`
+	RootBookmark string      `json:"root_bookmark"`
+	CreatedAt    pgtype.Date `json:"created_at"`
+	DeletedAt    string      `json:"deleted_at"`
 }
 
-type TaskLinkUserPreferences struct {
-	LinkId string `gorm:""`
-	UserId string `gorm:""`
-	Fold   bool   `gorm:""`
-}
-
-type TaskProps struct {
-	RevisionId           string  `gorm:""`
-	TaskId               string  `gorm:""`
-	UserId               string  `gorm:""`
-	Degree               int     `gorm:""`
-	Depth                int     `gorm:""`
-	CompletionPercentage float64 `gorm:""`
-}
-
-type TaskPermission struct {
+type ObjectivePermission struct {
 }
--- app/cmd/tasks/database/types.go
+++ app/cmd/tasks/database/types.go
@@ -3,21 +3,86 @@ package database
 import "regexp"
 
 type (
-	Did               string // Document Id
-	Iid               string // Item Id
+	UserId      string
+	ObjectiveId string 
+	VersionId   string
+	CommitId    string
+	ActionId    string
+	LinkId      string
+
 	NonNegativeNumber int
 )
 
 var uuid = regexp.MustCompile(`^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$`)
 
-func (v Did) Validate() bool {
+func (v UserId) Validate() bool {
 	return uuid.MatchString(string(v))
 }
 
-func (v Iid) Validate() bool {
+func (v ObjectiveId) Validate() bool {
+	return uuid.MatchString(string(v))
+}
+
+func (v VersionId) Validate() bool {
+	return uuid.MatchString(string(v))
+}
+
+func (v CommitId) Validate() bool {
+	return uuid.MatchString(string(v))
+}
+
+func (v ActionId) Validate() bool {
+	return uuid.MatchString(string(v))
+}
+
+func (v LinkId) Validate() bool {
 	return uuid.MatchString(string(v))
 }
 
 func (v NonNegativeNumber) Validate() bool {
 	return v >= 0
 }
+
+type OperationSummary string
+
+const (
+	TASK_CREATE               OperationSummary = "TASK_CREATE"
+	TASK_REORDER              OperationSummary = "TASK_REORDER"
+	TASK_DELETE               OperationSummary = "TASK_DELETE"
+	TASK_CONTENT_EDIT         OperationSummary = "TASK_CONTENT_EDIT"
+	TASK_MARK_COMPLETE        OperationSummary = "TASK_MARK_COMPLETE"
+	TASK_MARK_UNCOMPLETE      OperationSummary = "TASK_MARK_UNCOMPLETE"
+	NOTE_CREATE               OperationSummary = "NOTE_CREATE"
+	NOTE_EDIT                 OperationSummary = "NOTE_EDIT"
+	NOTE_DELETE               OperationSummary = "NOTE_DELETE"
+	COLLABORATION_ASSIGN      OperationSummary = "COLLABORATION_ASSIGN"
+	COLLABORATION_UNASSIGN    OperationSummary = "COLLABORATION_UNASSIGN"
+	COLLABORATION_RESTRICT    OperationSummary = "COLLABORATION_RESTRICT"
+	COLLABORATION_DERESTRICT  OperationSummary = "COLLABORATION_DERESTRICT"
+	COLLABORATION_CHANGE_ROLE OperationSummary = "COLLABORATION_CHANGE_ROLE"
+	HISTORY_ROLLBACK          OperationSummary = "HISTORY_ROLLBACK"
+	HISTORY_FASTFORWARD       OperationSummary = "HISTORY_FASTFORWARD"
+)
+
+type OperationStatus string
+
+const (
+	SERVER_ORIGINATED           OperationStatus = "SERVER_ORIGINATED"
+	IN_REVIEW                   OperationStatus = "IN_REVIEW"
+	PRIV_ACCEPTED               OperationStatus = "PRIV_ACCEPTED"
+	PRIV_REJECTED               OperationStatus = "PRIV_REJECTED"
+	APPLIED_FASTFORWARD         OperationStatus = "APPLIED_FASTFORWARD"
+	CONFLICT_DETECTED           OperationStatus = "CONFLICT_DETECTED"
+	MANAGER_SELECTION_IN_REVIEW OperationStatus = "MANAGER_SELECTION_IN_REVIEW"
+	MANAGER_SELECTION_ACCEPTED  OperationStatus = "MANAGER_SELECTION_ACCEPTED"
+	MANAGER_SELECTION_APPLIED   OperationStatus = "MANAGER_SELECTION_APPLIED"
+	MANAGER_SELECTION_REJECTED  OperationStatus = "MANAGER_SELECTION_REJECTED"
+)
+
+type LinkType string
+
+const (
+	Primary = LinkType("PRIMARY") // eg. When task owner break downs it
+	Remote  = LinkType("REMOTE")  // eg. Collaborated objective attached to local objectives
+	Private = LinkType("PRIVATE") //
+)
