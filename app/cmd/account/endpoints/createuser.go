6f1bb4731bc06e0df522fdc41f98e1628c06749d
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-03-10 14:18:37 +0300

    app/cmd/account: switches from username to firstname+surname on registration

--- app/cmd/account/endpoints/createuser.go
+++ app/cmd/account/endpoints/createuser.go
@@ -1,48 +1,38 @@
 package endpoints
 
 import (
+	"errors"
 	"fmt"
 	"log"
+	"logbook/cmd/account/app"
 	"logbook/cmd/account/database"
 	"logbook/internal/web/reqs"
 	"logbook/internal/web/validate"
 	"net/http"
-
-	"github.com/alexedwards/argon2id"
 )
 
 type CreateUserRequest struct {
-	Email       database.Email     `json:"email"`
-	NameSurname database.HumanName `json:"name_surname"`
-	Password    string             `json:"password"`
-	Username    database.Username  `json:"username"`
+	Email     database.Email     `json:"email"`
+	Password  string             `json:"password"`
+	Firstname database.HumanName `json:"firstname"`
+	Lastname  database.HumanName `json:"lastname"`
 }
 
 func (bq CreateUserRequest) validate() error {
 	return validate.All(map[string]validate.Validator{
-		"email":        bq.Email,
-		"name surname": bq.NameSurname,
-		"username":     bq.Username,
+		"email":     bq.Email,
+		"firstname": bq.Firstname,
+		"lastname":  bq.Lastname,
 	})
-}
-
-type CreateUserResponse struct{}
-
-var argon2idParams = &argon2id.Params{
-	Memory:      64 * 1024,
-	Iterations:  3,
-	Parallelism: 1,
-	SaltLength:  16,
-	KeyLength:   32,
 }
 
 /*
  * Objectives for this function
- * TODO: Sanitize user input
- * TODO: Produce unique salt and hash user password with it
- * TODO: Secure against timing-attacks
+ * DONE: Sanitize user input
+ * DONE: Produce unique salt and hash user password with it
+ * DONE: Secure against timing-attacks
  * TODO: Check anti-CSRF token
- * TODO: Check account duplication (attempt to register with same e-mail twice)
+ * DONE: Check account duplication (attempt to register with same e-mail twice)
  * TODO: Create first task
  * TODO: Create privilege table record for created task
  * TODO: Create operation table record for task creation
@@ -52,22 +42,32 @@ var argon2idParams = &argon2id.Params{
 func (e *Endpoints) CreateUser(w http.ResponseWriter, r *http.Request) {
 	bq, err := reqs.ParseRequest[CreateUserRequest](r)
 	if err != nil {
-		log.Println(fmt.Errorf("parsing request: %w", err))
+		log.Println(fmt.Errorf("binding: %w", err))
 		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 		return
 	}
 
 	if err := bq.validate(); err != nil {
-		log.Println(fmt.Errorf("validating request parameters: %w", err))
-		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		log.Println(fmt.Errorf("validation: %w", err))
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	hash, err := argon2id.CreateHash(bq.Password, argon2idParams)
+	err = e.app.Register(r.Context(), app.RegistrationParameters{
+		Firstname: bq.Firstname,
+		Lastname:  bq.Lastname,
+		Email:     bq.Email,
+		Password:  bq.Password,
+	})
 	if err != nil {
-		log.Println(fmt.Errorf("creating hash: %w", err))
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		log.Println(fmt.Errorf("app.Register: %w", err))
+		if errors.Is(err, app.ErrEmailExists) {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		} else {
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		}
 		return
 	}
 
+	w.WriteHeader(http.StatusOK)
 }
