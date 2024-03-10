6be5266fcb6fd815d8b8bdadc191e4e90f96e13b
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-03-10 16:31:03 +0300

    app/cmd/account: forgotten

--- app/cmd/account/endpoints/endpoints.go
+++ app/cmd/account/endpoints/endpoints.go
@@ -1,8 +1,13 @@
 package endpoints
 
+import "logbook/cmd/account/app"
+
 type Endpoints struct {
+	app *app.App
 }
 
-func New() *Endpoints {
-	return &Endpoints{}
+func New(a *app.App) *Endpoints {
+	return &Endpoints{
+		app: a,
+	}
 }
