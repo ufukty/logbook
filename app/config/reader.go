912910940176d38e74402f5a0393214814c2d5de
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-03-13 00:22:39 +0300

    app/config: adds logging

--- app/config/reader.go
+++ app/config/reader.go
@@ -12,24 +12,29 @@ import (
 )
 
 func Read(configpath string) Config {
-	var log = logger.NewLogger("ConfigReader")
-	
+	var l = logger.NewLogger("ConfigReader")
+
 	f, err := os.Open(configpath)
 	if err != nil {
-		log.Fatalln(fmt.Errorf("could not open config file: %w", err))
+		l.Fatalln(fmt.Errorf("could not open config file: %w", err))
 	}
 	config := Config{}
 	err = yaml.NewDecoder(f).Decode(&config)
 	if err != nil {
-		log.Fatalln(fmt.Errorf("could not decode config file: %w", err))
+		l.Fatalln(fmt.Errorf("could not decode config file: %w", err))
 	}
 
 	errs := reflux.FindZeroValues(config)
 	if len(errs) > 0 {
-		log.Fatalf(
+		l.Fatalf(
 			"config has missing values:\n%s\n",
 			strw.IndentLines(strings.Join(errs, "\n"), 4),
 		)
 	}
+
+	for _, line := range strings.Split(reflux.String(config), "\n") {
+		l.Println(line)
+	}
+
 	return config
 }
