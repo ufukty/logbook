e846043da4cbf976e241c8df40691600cb717066
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-04-12 19:42:28 +0300

    app/cmd/*: fix tests which performs migrations

--- app/cmd/objectives/database/database_test.go
+++ app/cmd/objectives/database/database_test.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"fmt"
+	"log"
 	"logbook/cmd/objectives/service"
 	"logbook/internal/utilities/run"
 	"testing"
@@ -14,17 +15,15 @@ func runMigration(cfg service.Config) error {
 		"-c", "DROP DATABASE IF EXISTS "+cfg.Database.Name+";",
 		"-c", "CREATE DATABASE "+cfg.Database.Name+";",
 	)
-	if output != "" {
-		return fmt.Errorf("dropping and recreating the application database: %s", output)
-	}
+	log.Println("dropping and recreating the application database:")
+	fmt.Println(output)
 	output = run.ExitAfterStderr("psql",
 		"-U", cfg.Database.User,
 		"-d", cfg.Database.Name,
 		"-f", "schema.sql",
 	)
-	if output != "" {
-		return fmt.Errorf("building the application database: %s", output)
-	}
+	log.Println("building the application database:")
+	fmt.Println(output)
 	return nil
 }
 
