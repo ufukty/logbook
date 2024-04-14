c1261b691f74336742df1b7bb9e0c245101c152a
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-04-14 23:57:48 +0300

    app/int/web/validate: adds emptiness check to StringBasics

--- app/internal/web/validate/utilities.go
+++ app/internal/web/validate/utilities.go
@@ -9,13 +9,16 @@ var (
 	ErrPattern = fmt.Errorf("content")
 	ErrLong    = fmt.Errorf("too long")
 	ErrShort   = fmt.Errorf("too short")
+	ErrEmpty   = fmt.Errorf("empty")
 )
 
 // for external use
 func StringBasics(s string, min, max int, pattern *regexp.Regexp) error {
-	if len(s) < min {
+	if l := len(s); l == 0 {
+		return ErrEmpty
+	} else if l < min {
 		return ErrShort
-	} else if len(s) > max {
+	} else if l > max {
 		return ErrLong
 	} else if !pattern.MatchString(string(s)) {
 		return ErrPattern
