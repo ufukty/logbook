63789e8e50e9e1982b5b125a8763af894e262d07
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-04-17 12:56:37 +0300

    app/test.sh

--- app/run.sh
+++ app/run.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 set -x
-set -m
-
-function cleanup() {
-    set +x
-    jobs -p | xargs -I {} kill -INT -{} || echo
-}
-
-trap cleanup EXIT
 
 for s in cmd/*; do
     /usr/local/go/bin/go test -timeout 10s -run '^TestMigration$' "logbook/$s/database" -v -count=1
     go run "logbook/$s" -a api.yml -d ../platform/local.yml -s "$s/testing.yml" &
 done
 
-sleep 2
-httpyac --all examples.rest
+wait
