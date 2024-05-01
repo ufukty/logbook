e9655aab9fc974b7299bfdc93a345ae29adfa945
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-05-01 10:54:15 +0300

    platform: adds two functions to autosource to replace make with shell scripts

--- platform/autosource.sh
+++ platform/autosource.sh
@@ -35,3 +35,20 @@ _ssh_completion() {
     return 0
 }
 complete -F _ssh_completion ssh
+
+is_newer_than_all() {
+    local TARGET="$1"
+    local -a SOURCES=("${@:2}")
+    test -e "$TARGET" || return 1
+    for SOURCE in "${SOURCES[@]}"; do
+        test -e "$SOURCE" || continue
+        test "$TARGET" -ot "$SOURCE" && return 1
+    done
+    return 0
+}
+export -f is_newer_than_all
+
+is_up_to_date() {
+    is_newer_than_all "$1" $(find . -type f | grep -v "$1")
+}
+export -f is_up_to_date
