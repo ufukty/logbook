860f040931d5b89012b0a8d97a121859b0ee4754
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-05-01 10:54:52 +0300

    platform/stage/image/base: replaces makefile with shell script by using is_up_to_date function

--- platform/stage/image/base/build.sh
+++ platform/stage/image/base/build.sh
@@ -1,5 +1,7 @@
 #!/usr/local/bin/bash
 
+test "$1" != "-B" && is_up_to_date .completion.timestamp && echo "up to date" && exit 0
+
 PS4="\033[36m$(grealpath --relative-to="$(dirname "$WORKSPACE")" "$(pwd -P)")\033[32m/\$(basename \"\${BASH_SOURCE}\"):\${LINENO}\033[0m\033[33m\${FUNCNAME[0]:+/\${FUNCNAME[0]}():}\033[0m "
 set -x
 set -e
@@ -15,47 +17,49 @@ FOLDER="$(basename "$PWD")"
 DROPLET_NAME="builder-${FOLDER:?}-$(date +%y-%m-%d-%H-%M-%S)"
 SNAPSHOT_NAME="build_${FOLDER:?}_$(date +%y_%m_%d_%H_%M_%S)"
 
-DROPLET="$(
-    doctl compute droplet create "${DROPLET_NAME:?}" \
-        --image "${BASE:?}" \
-        --region "${REGION:?}" \
-        --size "${SIZE:?}" \
-        --ssh-keys "${SSH_KEY_IDs:?}" \
-        --tag-name "${FOLDER:?}" \
-        --wait \
-        --verbose \
-        --no-header
-)"
-ID="$(echo "$DROPLET" | tail -n 1 | awk '{ print  $1 }')"
-IP="$(echo "$DROPLET" | tail -n 1 | awk '{ print  $3 }')"
+# DROPLET="$(
+#     doctl compute droplet create "${DROPLET_NAME:?}" \
+#         --image "${BASE:?}" \
+#         --region "${REGION:?}" \
+#         --size "${SIZE:?}" \
+#         --ssh-keys "${SSH_KEY_IDs:?}" \
+#         --tag-name "${FOLDER:?}" \
+#         --wait \
+#         --verbose \
+#         --no-header
+# )"
+# ID="$(echo "$DROPLET" | tail -n 1 | awk '{ print  $1 }')"
+# IP="$(echo "$DROPLET" | tail -n 1 | awk '{ print  $3 }')"
 
-cleanup() {
-    EC=$?
-    test $EC -eq 0 && test "$ID" && doctl compute droplet delete "$ID" --force
-    exit $EC
-}
+# cleanup() {
+#     EC=$?
+#     test $EC -eq 0 && test "$ID" && doctl compute droplet delete "$ID" --force
+#     exit $EC
+# }
 
-trap cleanup EXIT
+# trap cleanup EXIT
 
-ping -o "${IP:?}" && until ssh "root@${IP:?}" exit; do sleep 5; done # wait
+# ping -o "${IP:?}" && until ssh "root@${IP:?}" exit; do sleep 5; done # wait
 
-rsync --verbose --recursive -e ssh "./map" "root@${IP:?}:/root/"
+# rsync --verbose --recursive -e ssh "./map" "root@${IP:?}:/root/"
 
-export ANSIBLE_CONFIG="ansible/ansible.cfg"
-ansible-playbook -i "${IP:?}," -u root ansible/playbook.yml
+# export ANSIBLE_CONFIG="ansible/ansible.cfg"
+# ansible-playbook -i "${IP:?}," -u root ansible/playbook.yml
 
-# VPS_USERNAME="olwgtzjzhnvexhpr"
-# VPS_HOME="/home/${VPS_USERNAME:?}"
+# # VPS_USERNAME="olwgtzjzhnvexhpr"
+# # VPS_HOME="/home/${VPS_USERNAME:?}"
 
-doctl compute droplet-action snapshot "${ID:?}" \
-    --snapshot-name "${SNAPSHOT_NAME:?}" \
-    --wait \
-    --verbose
+# doctl compute droplet-action snapshot "${ID:?}" \
+#     --snapshot-name "${SNAPSHOT_NAME:?}" \
+#     --wait \
+#     --verbose
 
-SNAPSHOT_ID="$(doctl compute snapshot list | grep "$ID" | awk '{ print $1 }')" # do not use the action id from previous output
+# SNAPSHOT_ID="$(doctl compute snapshot list | grep "$ID" | awk '{ print $1 }')" # do not use the action id from previous output
 
-for TRANSFER_REGION in "${TRANSFER_REGIONS[@]}"; do
-    doctl compute image-action transfer "$SNAPSHOT_ID" --region "$TRANSFER_REGION" --wait
-done
+# for TRANSFER_REGION in "${TRANSFER_REGIONS[@]}"; do
+#     doctl compute image-action transfer "$SNAPSHOT_ID" --region "$TRANSFER_REGION" --wait
+# done
 
-doctl compute snapshot list | grep -e "$SNAPSHOT_ID" -e "Created at"
+# doctl compute snapshot list | grep -e "$SNAPSHOT_ID" -e "Created at"
+
+touch .completion.timestamp
