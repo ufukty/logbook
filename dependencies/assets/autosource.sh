23c3e37460189891b07b6f851c77f4dad6381dc2
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-05-10 13:33:09 +0300

    platform/autosource.sh change the name of autosource supported cd command to cde

--- dependencies/assets/autosource.sh
+++ dependencies/assets/autosource.sh
@@ -4,18 +4,18 @@ function _autosource() {
     local PARENTDIR
     PARENTDIR="$(dirname "$1")"
     test "$PARENTDIR" && test "$PARENTDIR" != "/" && test -d "$PARENTDIR" && _autosource "$PARENTDIR"
-    builtin cd "$1" || return
+    cd "$1" || return
     test -f "$1/autosource.sh" && echo "+ source $PWD/autosource.sh" && source "autosource.sh"
 }
 _autosource "$PWD"
 
-_cd() {
+cde() {
+    set +x
     local START_DIR
     START_DIR="$(pwd -P)"
-    builtin cd "$@" || return
+    cd "$@" || return
     set +E
     set +e
     _autosource "$PWD"
     OLDPWD="$START_DIR"
 }
-alias cd="_cd"
