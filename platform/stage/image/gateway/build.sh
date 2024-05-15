3e2046a6c77e9144d9b4482f47c3364d768914b2
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-05-15 18:21:42 +0300

    platform/stage/image/gateway: copies the image from deepthinker project

---
+++ platform/stage/image/gateway/build.sh
@@ -0,0 +1,66 @@
+#!/usr/local/bin/bash
+
+test "$1" != "-B" && is_up_to_date .completion.timestamp && echo "up to date" && exit 0
+
+PS4="\033[32m/\$(basename \"\${BASH_SOURCE}\"):\${LINENO}\033[0m\033[33m\${FUNCNAME[0]:+/\${FUNCNAME[0]}():}\033[0m "
+set -x
+set -v
+set -e
+
+VPS_SUDO_USER="olwgtzjzhnvexhpr"
+VPS_HOME="/home/${VPS_SUDO_USER:?}"
+IPTABLES_PUBLIC_ETHERNET_INTERFACE="eth0"
+IPTABLES_PRIVATE_ETHERNET_INTERFACE="eth1"
+
+BASE_IMAGE_PREFIX="build_internal"
+BASE_IMAGE_ID="$(doctl compute image list-user --format Name,ID --no-header | grep "^$BASE_IMAGE_PREFIX" | sort | tail -n 1 | awk '{ print $2 }')"
+REGION="fra1"
+SIZE="s-1vcpu-1gb"
+SSH_KEY_IDs="41814107"
+VPC_UUID="$(doctl vpcs list | grep logbook-fra1 | awk '{ print $1 }')"
+
+TRANSFER_REGIONS=("nyc3" "ams3")
+
+FOLDER="$(basename "$PWD")"
+DROPLET_NAME="builder-${FOLDER:?}-$(date +%y-%m-%d-%H-%M-%S)"
+SNAPSHOT_NAME="build_${FOLDER:?}_$(date +%y_%m_%d_%H_%M_%S)"
+
+DROPLET="$(doctl compute droplet create "${DROPLET_NAME:?}" --image "${BASE_IMAGE_ID:?}" --region "${REGION:?}" --size "${SIZE:?}" --ssh-keys "${SSH_KEY_IDs:?}" --tag-name "${FOLDER:?}" --vpc-uuid "${VPC_UUID:?}" --enable-private-networking --wait --verbose --no-header --format ID,PublicIPv4)"
+ID="$(echo "$DROPLET" | tail -n 1 | awk '{ print  $1 }')"
+IP="$(echo "$DROPLET" | tail -n 1 | awk '{ print  $2 }')"
+
+cleanup() {
+    EC=$?
+    test $EC -eq 0 && test "$ID" && doctl compute droplet delete "$ID" --force
+    tput bel
+    exit $EC
+}
+
+trap cleanup EXIT
+
+ping -o "${IP:?}" && until ssh "${VPS_SUDO_USER:?}@${IP:?}" exit; do sleep 5; done # wait
+
+rsync --verbose --recursive -e ssh "./files" "${VPS_SUDO_USER:?}@${IP:?}:${VPS_HOME:?}/"
+ssh "${VPS_SUDO_USER:?}@$IP" bash <<EOF
+    set -e
+    set -v
+    cd "${VPS_HOME:?}/files"
+    sudo --preserve-env \
+        IPTABLES_PUBLIC_ETHERNET_INTERFACE="${IPTABLES_PUBLIC_ETHERNET_INTERFACE:?}" \
+        IPTABLES_PRIVATE_ETHERNET_INTERFACE="${IPTABLES_PRIVATE_ETHERNET_INTERFACE:?}" \
+        bash golden-image.sh
+    cd "${VPS_HOME:?}"
+    rm -rf "${VPS_HOME:?}/files"
+    sudo shutdown -h now
+EOF
+
+doctl compute droplet-action snapshot "${ID:?}" --snapshot-name "${SNAPSHOT_NAME:?}" --wait --verbose
+SNAPSHOT_ID="$(doctl compute snapshot list | grep "$ID" | awk '{ print $1 }')" # do not use the action id from previous output
+
+for TRANSFER_REGION in "${TRANSFER_REGIONS[@]}"; do
+    doctl compute image-action transfer "$SNAPSHOT_ID" --region "$TRANSFER_REGION" --wait --verbose
+done
+
+doctl compute snapshot list | grep -e "$SNAPSHOT_ID" -e "Created at"
+
+touch .completion.timestamp
---
+++ platform/stage/image/gateway/files/golden-image.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# ---------------------------------------------------------------------------- #
+# Assertions
+# ---------------------------------------------------------------------------- #
+
+: "${IPTABLES_PRIVATE_ETHERNET_INTERFACE:?}"
+: "${IPTABLES_PUBLIC_ETHERNET_INTERFACE:?}"
+
+# ---------------------------------------------------------------------------- #
+# Commons
+# ---------------------------------------------------------------------------- #
+
+PS4="\033[32m/\$(basename \"\${BASH_SOURCE}\"):\${LINENO}\033[0m\033[33m\${FUNCNAME[0]:+/\${FUNCNAME[0]}():}\033[0m "
+set -x
+set -v
+set -e
+set -E
+
+PROVISIONER_FILES="$(pwd -P)"
+
+function retry() {
+    local COUNTER=0
+    until "$@"; do
+        EC=$?
+        COUNTER=$((COUNTER + 1))
+        test $COUNTER -ge 60 && exit $EC
+        sleep 10
+    done
+}
+
+function apt_update() { retry apt-get update; }
+function restart_journald() { systemctl restart systemd-journald; }
+function assert_sudo() { test $EUID -eq 0; }
+function remove_password_change_requirement() { sed --in-place -E 's/root:(.*):0:0:(.*):/root:\1:18770:0:\2:/g' /etc/shadow; }
+function wait_cloud_init() { cloud-init status --wait >/dev/null; }
+function check_tun_availability() { test -e /dev/net/tun; }
+function deploy_provisioner_files() {
+    chmod 700 -R "$PROVISIONER_FILES/map"
+    chown root:root -R "$PROVISIONER_FILES/map"
+    rsync --verbose --recursive "$PROVISIONER_FILES/map/" "/"
+    rm -rfv "$PROVISIONER_FILES/map"
+}
+
+export DEBIAN_FRONTEND=noninteractive
+
+# ---------------------------------------------------------------------------- #
+# Tasks
+# ---------------------------------------------------------------------------- #
+
+function iptables_configure() {
+    sed --in-place \
+        -e "s/{{PRIVATE_ETHERNET_INTERFACE}}/${IPTABLES_PRIVATE_ETHERNET_INTERFACE:?}/g" \
+        -e "s/{{PUBLIC_ETHERNET_INTERFACE}}/${IPTABLES_PUBLIC_ETHERNET_INTERFACE:?}/g" \
+        "/etc/iptables/iptables-rules.v4"
+
+    systemctl daemon-reload
+    systemctl enable iptables-activation
+    systemctl restart iptables-activation
+}
+
+# ---------------------------------------------------------------------------- #
+# Main
+# ---------------------------------------------------------------------------- #
+
+assert_sudo
+restart_journald
+remove_password_change_requirement
+wait_cloud_init
+
+deploy_provisioner_files
+
+iptables_configure
