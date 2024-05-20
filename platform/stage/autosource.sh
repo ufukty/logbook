0f99ab9823c8c7441aba378d6d916690b75017a8
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-05-20 22:32:38 +0300

    platform/stage/autosource: renames digitalocean images related commands

--- platform/stage/autosource.sh
+++ platform/stage/autosource.sh
@@ -170,7 +170,13 @@ generate-keys() (
 
 # MARK: Digitalocean
 
-image-clean() {
+do-up-to-date-images() {
+    local ALL
+    ALL="$(doctl compute snapshot list --format Name,ID --no-header | grep build | grep -v base | sort -r | gsed -E 's/[ ]+/ /g')"
+    echo "$ALL" | gsed -E 's/(build_([^_]+)_([0-9_]{17}).*)/\1 \2/g' | sort -r | uniq -f 2 | cut -d ' ' -f 1-2 | gsed -E 's/^([^\ ]+) (.*)/\2 \1/g'
+}
+
+do-clean-outdated-images() {
     set -E
     local ALL
     local TOKEEP
