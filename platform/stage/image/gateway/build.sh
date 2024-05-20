4252cd9dcdd81aba52e1851dfcde0ecf754f4cf7
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-05-20 11:19:04 +0300

    platform/stage/image/gateway: updates debug related details to reflect changes in image/internal/build.sh

--- platform/stage/image/gateway/build.sh
+++ platform/stage/image/gateway/build.sh
@@ -2,11 +2,12 @@
 
 test "$1" != "-B" && is_up_to_date .completion.timestamp && echo "up to date" && exit 0
 
-PS4="\033[32m/\$(basename \"\${BASH_SOURCE}\"):\${LINENO}\033[0m\033[33m\${FUNCNAME[0]:+/\${FUNCNAME[0]}():}\033[0m "
-set -x
-set -v
-set -e
+PS4='\033[32m$(basename "${BASH_SOURCE}"):${LINENO}\033[0m\033[33m${FUNCNAME[0]:+/${FUNCNAME[0]}():}\033[0m '
+set -v -x -e
 
+# ---------------------------------------------------------------------------- #
+# Values
+# ---
 VPS_SUDO_USER="olwgtzjzhnvexhpr"
 VPS_HOME="/home/${VPS_SUDO_USER:?}"
 IPTABLES_PUBLIC_ETHERNET_INTERFACE="eth0"
@@ -25,18 +26,27 @@ FOLDER="$(basename "$PWD")"
 DROPLET_NAME="builder-${FOLDER:?}-$(date +%y-%m-%d-%H-%M-%S)"
 SNAPSHOT_NAME="build_${FOLDER:?}_$(date +%y_%m_%d_%H_%M_%S)"
 
+# ---------------------------------------------------------------------------- #
+# Creation
+# ---------------------------------------------------------------------------- #
+
 DROPLET="$(doctl compute droplet create "${DROPLET_NAME:?}" --image "${BASE_IMAGE_ID:?}" --region "${REGION:?}" --size "${SIZE:?}" --ssh-keys "${SSH_KEY_IDs:?}" --tag-name "${FOLDER:?}" --vpc-uuid "${VPC_UUID:?}" --enable-private-networking --wait --verbose --no-header --format ID,PrivateIPv4)"
 ID="$(echo "$DROPLET" | tail -n 1 | awk '{ print  $1 }')"
 IP="$(echo "$DROPLET" | tail -n 1 | awk '{ print  $2 }')"
 
 cleanup() {
     EC=$?
-    test $EC -eq 0 && test "$ID" && doctl compute droplet delete "$ID" --force
+    test "$ID" && test "$1" != "-d" && doctl compute droplet delete "$ID" --force
+    test $EC -eq 0 && touch .completion.timestamp
     tput bel
     exit $EC
 }
 
 trap cleanup EXIT
+
+# ---------------------------------------------------------------------------- #
+# Provisioning
+# ---------------------------------------------------------------------------- #
 
 ping -o "${IP:?}" && until ssh "${VPS_SUDO_USER:?}@${IP:?}" exit; do sleep 5; done # wait
 
@@ -53,6 +63,10 @@ ssh "${VPS_SUDO_USER:?}@$IP" bash <<EOF
     rm -rf "${VPS_HOME:?}/files"
     sudo shutdown -h now
 EOF
+
+# ---------------------------------------------------------------------------- #
+# Snapshot
+# ---------------------------------------------------------------------------- #
 
 doctl compute droplet-action snapshot "${ID:?}" --snapshot-name "${SNAPSHOT_NAME:?}" --wait --verbose
 SNAPSHOT_ID="$(doctl compute snapshot list | grep "$ID" | awk '{ print $1 }')" # do not use the action id from previous output
