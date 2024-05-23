6a1c2b0b6efc6ceda03586dd146b74ecf3187da2
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-05-23 11:15:35 +0300

    platform/stage/image/base: syncs the changes in build.sh according to other images

--- platform/stage/image/base/build.sh
+++ platform/stage/image/base/build.sh
@@ -2,19 +2,27 @@
 
 test "$1" != "-B" && is_up_to_date .completion.timestamp && echo "up to date" && exit 0
 
-set -v
-set -e
+PS4='\033[32m$(basename "${BASH_SOURCE}"):${LINENO}\033[0m\033[33m${FUNCNAME[0]:+/${FUNCNAME[0]}():}\033[0m '
+set -v -x -e
+
+# ---------------------------------------------------------------------------- #
+# Values
+# ---------------------------------------------------------------------------- #
 
 BASE="ubuntu-22-04-x64"
 REGION="fra1"
 SIZE="s-1vcpu-1gb"
 SSH_KEY_IDs="41814107"
 
-TRANSFER_REGIONS=("nyc3" "ams3")
+TRANSFER_REGIONS=() # ("nyc3" "ams3")
 
 FOLDER="$(basename "$PWD")"
 DROPLET_NAME="builder-${FOLDER:?}-$(date +%y-%m-%d-%H-%M-%S)"
 SNAPSHOT_NAME="build_${FOLDER:?}_$(date +%y_%m_%d_%H_%M_%S)"
+
+# ---------------------------------------------------------------------------- #
+# Creation
+# ---------------------------------------------------------------------------- #
 
 DROPLET="$(doctl compute droplet create "${DROPLET_NAME:?}" --image "${BASE:?}" --region "${REGION:?}" --size "${SIZE:?}" --ssh-keys "${SSH_KEY_IDs:?}" --tag-name "${FOLDER:?}" --wait --verbose --no-header)"
 ID="$(echo "$DROPLET" | tail -n 1 | awk '{ print  $1 }')"
@@ -22,12 +30,17 @@ IP="$(echo "$DROPLET" | tail -n 1 | awk '{ print  $3 }')"
 
 cleanup() {
     EC=$?
-    test $EC -eq 0 && test "$ID" && doctl compute droplet delete "$ID" --force
+    test "$ID" && test "$1" != "-d" && doctl compute droplet delete "$ID" --force
+    test $EC -eq 0 && touch .completion.timestamp
     tput bel
     exit $EC
 }
 
 trap cleanup EXIT
+
+# ---------------------------------------------------------------------------- #
+# Provisioning
+# ---------------------------------------------------------------------------- #
 
 ping -o "${IP:?}" && until ssh "root@${IP:?}" exit; do sleep 5; done # wait
 
@@ -35,11 +48,11 @@ rsync --verbose --recursive -e ssh "./map" "root@${IP:?}:/root/"
 
 export ANSIBLE_CONFIG="ansible/ansible.cfg"
 ansible-playbook -i "${IP:?}," -u root ansible/playbook.yml
-
-# VPS_USERNAME="olwgtzjzhnvexhpr"
-# VPS_HOME="/home/${VPS_USERNAME:?}"
-
 ssh "olwgtzjzhnvexhpr@$IP" sudo shutdown -h now
+
+# ---------------------------------------------------------------------------- #
+# Snapshot
+# ---------------------------------------------------------------------------- #
 
 doctl compute droplet-action snapshot "${ID:?}" --snapshot-name "${SNAPSHOT_NAME:?}" --wait --verbose
 
