8c2803e267e27b52265541410b8d923dfb6d03d5
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-05-26 23:33:40 +0300

    platform/stage: adds do-images function to autosource

--- platform/stage/autosource.sh
+++ platform/stage/autosource.sh
@@ -187,6 +187,14 @@ rotate-cryptographic-keys() (
 
 # MARK: Digitalocean
 
+do-images() {
+    ALL="$(doctl compute snapshot list --format CreatedAt,ID,Name)"
+    ALL_P="$(echo "$ALL" | tail -n +2 | gsed -E -e 's/[\ \t]+/ /g' -e 's/_/ /g')"
+    KINDS="$(echo "$ALL_P" | cut -d ' ' -f 4 | sort | uniq)"
+    UPTODATE_IDs="$(echo "$KINDS" | while read KIND; do echo "$ALL_P" | grep "$KIND" | tail -n 1 | cut -d ' ' -f 2; done)"
+    echo "$ALL" | while read IMAGE; do if echo "$IMAGE" | grep "$UPTODATE_IDs" >/dev/null 2>&1; then echo "$IMAGE" | green; else echo "$IMAGE"; fi; done
+}
+
 do-up-to-date-images() {
     local ALL
     ALL="$(doctl compute snapshot list --format Name,ID --no-header | grep build | grep -v base | sort -r | gsed -E 's/[ ]+/ /g')"
