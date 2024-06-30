b31f96e3eba3ea2e608787b5d8f4e968730ae506
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-06-30 12:08:09 +0300

    app/config/api: adopts gonfique 1.6 directives for api mapping

--- app/config/api/methods.go
+++ app/config/api/methods.go
@@ -1,15 +1,52 @@
 package api
 
-import "path/filepath"
+import (
+	"path/filepath"
+)
 
-func joinPaths(segment ...Path) Path {
-	j := ""
-	for _, s := range segment {
-		j = filepath.Join(j, string(s))
-	}
-	return Path(j)
+type Addressable interface {
+	GetPath() string
 }
 
-func (p Path) Join(n Path) Path {
-	return joinPaths(p, n)
+func Join(addrs ...Addressable) string {
+	j := ""
+	for _, s := range addrs {
+		j = filepath.Join(j, s.GetPath())
+	}
+	return j
+}
+
+type Child interface {
+	GetParent() any
+}
+
+// endpoint/../.. = service
+// service/../.. = gateway
+func twoup(a Addressable) Addressable {
+	var u Addressable = a
+	for i := 0; i < 2; i++ {
+		c1, ok := u.(Child)
+		if !ok {
+			return nil
+		}
+		p1 := c1.GetParent()
+		u1, ok := p1.(Addressable)
+		if !ok {
+			return nil
+		}
+		u = u1
+	}
+	return u
+}
+
+func PathFromInternet(a Addressable) string {
+	addressables := []Addressable{a}
+	for i := 0; i < 2; i++ { // endpoint > service (../) > gateway (../../)
+		up := twoup(addressables[len(addressables)-1])
+		if up == nil {
+			break
+		}
+		addressables = append(addressables, up)
+	}
+	return Join(addressables...)
 }
