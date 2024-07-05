0f99ffe6fc7cc934e266d9b8976907573fefc8a2
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-07-05 15:36:41 +0300

    app/test.sh: fixes argument list of services

--- app/test.sh
+++ app/test.sh
@@ -3,21 +3,48 @@ set -x
 set -m
 
 function cleanup() {
-    set +x
-    jobs -p | xargs -I {} kill -INT -{} || echo
+  set +x
+  jobs -p | xargs -I {} kill -INT -{} || echo
 }
 
 trap cleanup EXIT
 
-for s in cmd/*; do
-    /usr/local/go/bin/go test -timeout 10s -run '^TestMigration$' "logbook/$s/database" -v -count=1
-    go run "logbook/$s" \
-        -a api.yml \
-        -d ../platform/local.yml \
-        -s "$s/testing.yml" \
-        -cert "../platform/local/tls/localhost.crt" \
-        -key "../platform/local/tls/localhost.key" &
-done
+function service() {
+  SERVICENAME="${1:?}"
+  /usr/local/go/bin/go test -timeout 10s -run '^TestMigration$' "logbook/cmd/${SERVICENAME}/database" -v -count=1
+  go run "logbook/cmd/${SERVICENAME}" \
+    -e local \
+    -api api.yml \
+    -deployment ../platform/local.yml \
+    -service "cmd/${SERVICENAME}/local.yml" \
+    -cert "../platform/local/tls/localhost.crt" \
+    -key "../platform/local/tls/localhost.key"
+}
+
+function gateway() {
+  go run "logbook/cmd/gateway" \
+    -e local \
+    -api api.yml \
+    -deployment ../platform/local.yml \
+    -discovery "internal/web/discovery/models/local/service_discovery.yml" \
+    -cert "../platform/local/tls/localhost.crt" \
+    -key "../platform/local/tls/localhost.key"
+}
+
+function nextcolor() {
+  echo $(($(jobs -p | wc -l) % 6 + 31))
+}
+
+function prefix() {
+  PREFIX="${1:?}"
+  COLOR="${2:?}"
+  esc=$(printf '\033')
+  gsed -E "s/^(.*)$/${esc}\[${COLOR}m${PREFIX}:${esc}\[0m \1/g"
+}
+
+service account 2>&1 | prefix account "$(nextcolor)" &
+service objectives 2>&1 | prefix objectives "$(nextcolor)" &
+gateway 2>&1 | prefix gateway "$(nextcolor)" &
 
 sleep 4
 httpyac api.http --all --insecure
