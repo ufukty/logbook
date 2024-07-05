ad707d61bfa42fe54fa0e45c8f86760a259141ee
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-07-05 08:37:15 +0300

    app/int/web/discovery: replaces hardcoded strings with enums

--- app/internal/web/discovery/sd_test.go
+++ app/internal/web/discovery/sd_test.go
@@ -9,13 +9,13 @@ import (
 
 func TestServiceDiscoveryStage(t *testing.T) {
 	var tcs = map[models.Environment]string{
-		"local": "models/local/service_discovery.yml",
-		"stage": "models/stage/service_discovery.json",
+		models.Local: "models/local/service_discovery.yml",
+		models.Stage: "models/stage/service_discovery.json",
 	}
 	for env, tc := range tcs {
 		t.Run(string(env), func(t *testing.T) {
 			sd := New(env, tc, time.Second*5)
-			ips, err := sd.ServicePool("objectives")
+			ips, err := sd.ServicePool(models.Gateway)
 			if err != nil {
 				t.Fatal(fmt.Errorf("act: %w", err))
 			}
