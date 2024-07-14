7b2304d4152fcb5b18811d007e5dbb11344dcf6b
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-07-14 14:41:39 +0300

    app/models: complies requests.StringAssignable interface for Service type

--- app/models/services.go
+++ app/models/services.go
@@ -1,5 +1,7 @@
 package models
 
+import "fmt"
+
 type Service string
 
 var (
@@ -8,3 +10,16 @@ var (
 	Internal   = Service("internal")
 	Objectives = Service("objectives")
 )
+
+func (s *Service) Set(v string) error {
+	switch v {
+	case string(Account),
+		string(Discovery),
+		string(Internal),
+		string(Objectives):
+		*s = Service(v)
+	default:
+		return fmt.Errorf("invalid service name: %s", v)
+	}
+	return nil
+}
