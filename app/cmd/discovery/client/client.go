1ddf5e441bfca4dbf61f6849b560b7c1acc183b3
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-07-18 21:01:21 +0300

    app/cmd/discovery/client: inits. supports querying service from both the gateway and consumers with 'servicepath' parameter

--- app/cmd/discovery/client/client.go
+++ app/cmd/discovery/client/client.go
@@ -1,29 +1,63 @@
 package discovery
 
 import (
+	"fmt"
 	"logbook/cmd/discovery/endpoints"
+	"logbook/config/api"
 	"logbook/internal/web/balancer"
 	"logbook/internal/web/requests"
+	"net/http"
+	"path/filepath"
 )
 
 type Client struct {
-	lb *balancer.LoadBalancer
+	lb          *balancer.LoadBalancer
+	servicepath string
+	servicecfg  api.Internal
 }
 
-func NewClient(lb *balancer.LoadBalancer) *Client {
+// servicepath => https://ip:port/(servicepath)/endpoint, which changes when gateway used
+func NewClient(apicfg *api.Config, lb *balancer.LoadBalancer, servicepath string) *Client {
 	return &Client{
-		lb: lb,
+		servicepath: servicepath,
+		servicecfg:  apicfg.Internal,
+		lb:          lb,
 	}
 }
 
-func (c Client) RegisterInstance(bq *endpoints.RegisterInstanceRequest) (*endpoints.RegisterInstanceResponse, error) {
-	return requests.Send[endpoints.RegisterInstanceRequest, endpoints.RegisterInstanceResponse](c.lb.Next(), bq)
+func (c *Client) RegisterInstance(bq *endpoints.RegisterInstanceRequest) (*endpoints.RegisterInstanceResponse, error) {
+	instance, err := c.lb.Next()
+	if err != nil {
+		return nil, fmt.Errorf("getting the next service instance: %w", err)
+	}
+	url := filepath.Join(instance.String(), c.servicepath, c.servicecfg.Services.Discovery.Endpoints.RecheckInstance.Path)
+	bs := &endpoints.RegisterInstanceResponse{}
+	err = requests.Send(url, c.servicecfg.Services.Discovery.Endpoints.RecheckInstance.Method, bq, bs)
+	if err != nil {
+		return nil, fmt.Errorf("sending over bindings: %w", err)
+	}
+	return bs, err
 }
 
-func (c Client) RecheckInstance(bq *endpoints.RecheckInstanceRequest) (*endpoints.RecheckInstanceResponse, error) {
-	return requests.Send[endpoints.RecheckInstanceRequest, endpoints.RecheckInstanceResponse](c.lb.Next(), bq)
+func (c *Client) RecheckInstance(bq *endpoints.RecheckInstanceRequest) (*http.Response, error) {
+	instance, err := c.lb.Next()
+	if err != nil {
+		return nil, fmt.Errorf("loadbalancer: %w", err)
+	}
+	url := filepath.Join(instance.String(), c.servicepath, c.servicecfg.Services.Discovery.Endpoints.RecheckInstance.Path)
+	return requests.SendRaw(url, c.servicecfg.Services.Discovery.Endpoints.RecheckInstance.Method, bq)
 }
 
-func (c Client) ListInstances(bq *endpoints.ListInstancesRequest) (*endpoints.ListInstancesResponse, error) {
-	return requests.Send[endpoints.ListInstancesRequest, endpoints.ListInstancesResponse](c.lb.Next(), bq)
+func (c *Client) ListInstances(bq *endpoints.ListInstancesRequest) (*endpoints.ListInstancesResponse, error) {
+	instance, err := c.lb.Next()
+	if err != nil {
+		return nil, fmt.Errorf("getting the next service instance: %w", err)
+	}
+	url := filepath.Join(instance.String(), c.servicepath, c.servicecfg.Services.Discovery.Endpoints.RecheckInstance.Path)
+	bs := &endpoints.ListInstancesResponse{}
+	err = requests.Send(url, c.servicecfg.Services.Discovery.Endpoints.RecheckInstance.Method, bq, bs)
+	if err != nil {
+		return nil, fmt.Errorf("sending over bindings: %w", err)
+	}
+	return bs, err
 }
