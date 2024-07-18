30bcc570a25cabcfb3c6cc2922e9b3ae4db5310d
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-07-18 21:03:01 +0300

    app/models: adds Instance.String to ease setting server urls from clients

--- app/models/network.go
+++ app/models/network.go
@@ -11,13 +11,21 @@ type Instance struct {
 	Port    int    `json:"port"`
 }
 
-func (i Instance) Url() url.URL {
-	schema := "http"
+func (i Instance) Schema() string {
 	if i.Tls {
-		schema = "https"
+		return "https"
+	} else {
+		return "http"
 	}
+}
+
+func (i Instance) Url() url.URL {
 	return url.URL{
-		Scheme: schema,
+		Scheme: i.Schema(),
 		Host:   fmt.Sprintf(i.Address, i.Port),
 	}
+}
+
+func (i Instance) String() string {
+	return fmt.Sprintf("%s://%s:%d", i.Schema(), i.Address, i.Port)
 }
