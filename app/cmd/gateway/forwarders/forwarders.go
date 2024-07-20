65d623acf2a967335969e84b3bbc575bff6b4fec
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-07-20 09:13:33 +0300

    app/cmd/gateway: preps for integrating the api gateway with the internal gateway and service discovery

---
+++ app/cmd/gateway/forwarders/forwarders.go
@@ -0,0 +1,58 @@
+package forwarders
+
+import (
+	discovery "logbook/cmd/discovery/client"
+	"logbook/config/api"
+	"logbook/config/deployment"
+	"logbook/internal/args"
+	"logbook/internal/web/balancer"
+	"logbook/internal/web/discoveryfile"
+	"logbook/models"
+	"net/http"
+	"path/filepath"
+	"time"
+)
+
+// Forwarder forwards requests to an instance of the target service
+// The service's address listed through discovery service
+// Discovery service is accessed through internal gateway.
+// Internal gateway might have multiple instances, addresses known by config-based service discovery
+type Forwarders struct {
+	// to stop tickers later
+	internalstore   *discoveryfile.FileReader // config-based service discovery
+	discoverystore  *discovery.DiscoveryStore // self-registration based service discovery client
+	objectivesstore *discovery.DiscoveryStore
+}
+
+// instances of services listed
+func New(flags *args.GatewayArgs, deplcfg *deployment.Config, apicfg *api.Config) *Forwarders {
+	internalstore := discoveryfile.NewFileReader(flags.Discovery, time.Second, discoveryfile.ServiceParams{
+		Port: deplcfg.Ports.Internal,
+		Tls:  true,
+	})
+	discoveryctl := discovery.NewClient(
+		apicfg,
+		balancer.New(internalstore),
+		filepath.Join(apicfg.Internal.Path, apicfg.Internal.Services.Discovery.Path),
+	)
+
+	return &Forwarders{
+		internalstore:   internalstore,
+		discoverystore:  discovery.NewDiscoveryStore(discoveryctl, models.Discovery),
+		objectivesstore: discovery.NewDiscoveryStore(discoveryctl, models.Objectives),
+	}
+}
+
+func (f *Forwarders) Stop() {
+	f.internalstore.Stop()
+	f.discoverystore.Stop()
+	f.objectivesstore.Stop()
+}
+
+func (f *Forwarders) Objectives(w http.ResponseWriter, r *http.Request) {
+	
+}
+
+func (f *Forwarders) Account(w http.ResponseWriter, r *http.Request) {
+
+}
--- app/cmd/gateway/main.go
+++ app/cmd/gateway/main.go
@@ -3,15 +3,9 @@ package main
 import (
 	"fmt"
 	"log"
-	discovery "logbook/cmd/discovery/client"
 	"logbook/cmd/gateway/cfgs"
-	"logbook/config/api"
-	"logbook/internal/web/balancer"
-	"logbook/internal/web/discoveryfile"
-	"logbook/internal/web/forwarder"
+	"logbook/cmd/gateway/forwarders"
 	"logbook/internal/web/router"
-	"logbook/models"
-	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -22,28 +16,8 @@ func mainerr() error {
 		return fmt.Errorf("reading configs: %w", err)
 	}
 
-	is := discoveryfile.NewFileReader(flags.Discovery, time.Second, discoveryfile.ServiceParams{
-		Port: deplcfg.Ports.Internal,
-		Tls:  true,
-	})
-	defer is.Stop()
-	lb := balancer.New(is)
-	dsctl := discovery.NewClient(lb)
-	
-	// FIXME:
-	dsctl.ListInstances()
-	
-	// FIXME:
-	objectives, err := forwarder.New(, models.Objectives, api.PathFromInternet(apicfg.Public.Services.Objectives))
-	if err != nil {
-		return fmt.Errorf("creating forwarder for objectives: %w", err)
-	}
-	
-	// FIXME:
-	account, err := forwarder.New(, models.Account, api.PathFromInternet(apicfg.Public.Services.Account))
-	if err != nil {
-		return fmt.Errorf("creating forwarder for account: %w", err)
-	}
+	fws := forwarders.New(flags, deplcfg, apicfg)
+	defer fws.Stop()
 
 	router.StartServer(router.ServerParameters{
 		Router:  deplcfg.Router,
@@ -53,8 +27,8 @@ func mainerr() error {
 	}, func(r *mux.Router) {
 		r = r.UseEncodedPath()
 		sub := r.PathPrefix(apicfg.Public.Path).Subrouter()
-		sub.PathPrefix(apicfg.Public.Services.Objectives.Path).HandlerFunc(objectives.Handler)
-		sub.PathPrefix(apicfg.Public.Services.Account.Path).HandlerFunc(account.Handler)
+		sub.PathPrefix(apicfg.Public.Services.Account.Path).HandlerFunc(fws.Account)
+		sub.PathPrefix(apicfg.Public.Services.Objectives.Path).HandlerFunc(fws.Objectives)
 	})
 
 	return nil
