436420cdff415a44d11a3dba6415e38c2e0b79c3
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-07-22 12:11:22 +0300

    feature(cmd/objectives): adds `Client` methods for endpoints `MarkComplete`, `GetPlacementArray`

--- app/cmd/objectives/client/client.go
+++ app/cmd/objectives/client/client.go
@@ -23,6 +23,20 @@ func NewClient(lb *balancer.LoadBalancer, apicfg *api.Config) *Client {
 	}
 }
 
+func (c *Client) MarkComplete(bq *endpoints.MarkCompleteRequest) (*endpoints.MarkCompleteResponse, error) {
+	instance, err := c.lb.Next()
+	if err != nil {
+		return nil, fmt.Errorf("LoadBalancer.Next: %w", err)
+	}
+	url := filepath.Join(instance.String(), c.servicepath, c.servicecfg.Endpoints.Mark.Path)
+	bs := &endpoints.MarkCompleteResponse{}
+	err = requests.Send(url, c.servicecfg.Endpoints.Mark.Method, bq, bs)
+	if err != nil {
+		return nil, fmt.Errorf("requests.Send: %w", err)
+	}
+	return bs, err
+}
+
 func (c *Client) CreateObjective(bq *endpoints.CreateObjectiveRequest) (*endpoints.CreateObjectiveResponse, error) {
 	instance, err := c.lb.Next()
 	if err != nil {
@@ -45,6 +59,20 @@ func (c *Client) ReattachObjective(bq *endpoints.ReattachObjectiveRequest) (*end
 	url := filepath.Join(instance.String(), c.servicepath, c.servicecfg.Endpoints.Attach.Path)
 	bs := &endpoints.ReattachObjectiveResponse{}
 	err = requests.Send(url, c.servicecfg.Endpoints.Attach.Method, bq, bs)
+	if err != nil {
+		return nil, fmt.Errorf("requests.Send: %w", err)
+	}
+	return bs, err
+}
+
+func (c *Client) GetPlacementArray(bq *endpoints.GetPlacementArrayRequest) (*endpoints.GetPlacementArrayResponse, error) {
+	instance, err := c.lb.Next()
+	if err != nil {
+		return nil, fmt.Errorf("LoadBalancer.Next: %w", err)
+	}
+	url := filepath.Join(instance.String(), c.servicepath, c.servicecfg.Endpoints.Placement.Path)
+	bs := &endpoints.GetPlacementArrayResponse{}
+	err = requests.Send(url, c.servicecfg.Endpoints.Placement.Method, bq, bs)
 	if err != nil {
 		return nil, fmt.Errorf("requests.Send: %w", err)
 	}
