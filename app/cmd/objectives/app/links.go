7751addd09fdca44f5a15bd99e02a7916fd584fe
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-08-04 19:18:29 +0300

    app/cmd/objectives/app: implements `ListActivePathToRock` which allows the frontend to send only the updated objective's `ovid` without its parents/ascendants to apply `bubblink`

--- app/cmd/objectives/app/links.go
+++ app/cmd/objectives/app/links.go
@@ -5,8 +5,46 @@ import (
 	"fmt"
 	"logbook/cmd/objectives/database"
 	"logbook/models"
-	columns "logbook/models/columns"
+	"logbook/models/columns"
+	"slices"
+
+	"github.com/jackc/pgx/v5"
 )
+
+var ErrLeftBehind = fmt.Errorf("the objective is either directly or eventually linked to an objective which its version is left behind")
+
+// returns a path from the node to the root: [subject, active-ascendants, ..., rock] or [ErrLeftBehind]
+func (a *App) ListActivePathToRock(ctx context.Context, subject models.Ovid) ([]models.Ovid, error) {
+	active, err := a.queries.SelectActive(ctx, subject.Oid)
+	if err != nil {
+		return nil, fmt.Errorf("SelectActive(%s): %w", subject.Oid, err)
+	}
+	if subject.Vid != active.Vid {
+		return nil, ErrLeftBehind
+	}
+
+	parents, err := a.queries.SelectUpperLinks(ctx, database.SelectUpperLinksParams{
+		SubOid: subject.Oid,
+		SubVid: subject.Vid,
+	})
+	if err == pgx.ErrNoRows {
+		return []models.Ovid{subject}, nil // assuming it is the rock or an orphaned subtree
+	} else if err != nil {
+		return nil, fmt.Errorf("SelectUpperLinks(%s): %w", subject, err)
+	}
+
+	for _, parent := range parents {
+		path, err := a.ListActivePathToRock(ctx, models.Ovid{parent.SupOid, parent.SupVid})
+		if err == ErrLeftBehind {
+			continue // try other parents
+		} else if err != nil {
+			return nil, fmt.Errorf("ListActivePathToRock(%s): %w", parent, err)
+		} else {
+			return slices.Insert(path, 0, subject), nil
+		}
+	}
+	return nil, ErrLeftBehind
+}
 
 func (a *App) ListObjectiveAncestry(ctx context.Context, ovid models.Ovid) ([]models.Ovid, error) {
 	anc := []models.Ovid{}
