396eec5d97ef263883fba464a62671f7952ba508
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-08-04 19:30:23 +0300

    app/cmd/objectives/app: removes `ListObjectiveAncestry` as `ListActivePathToRock` replaces it

--- app/cmd/objectives/app/links.go
+++ app/cmd/objectives/app/links.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"logbook/cmd/objectives/database"
 	"logbook/models"
-	"logbook/models/columns"
 	"slices"
 
 	"github.com/jackc/pgx/v5"
@@ -44,28 +43,4 @@ func (a *App) ListActivePathToRock(ctx context.Context, subject models.Ovid) ([]
 		}
 	}
 	return nil, ErrLeftBehind
-}
-
-func (a *App) ListObjectiveAncestry(ctx context.Context, ovid models.Ovid) ([]models.Ovid, error) {
-	anc := []models.Ovid{}
-	c := ovid
-	for limit := 0; true; limit++ {
-		l, err := a.queries.SelectTheUpperLink(ctx, database.SelectTheUpperLinkParams{
-			SubOid: c.Oid,
-			SubVid: c.Vid,
-		})
-		if err != nil {
-			return nil, fmt.Errorf("db.SelectTheUpperLink(%q, %q): %w", c.Oid, c.Vid, err)
-		}
-		c.Oid = l.SupOid
-		c.Vid = l.SupVid
-		if c.Oid == columns.ZeroObjectId {
-			break
-		}
-		anc = append(anc, c)
-		if limit == 100 {
-			return nil, fmt.Errorf("depth limit (100) is reached")
-		}
-	}
-	return anc, nil
 }
