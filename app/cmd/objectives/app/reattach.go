f1f486203b236f5bceb19815194903ca83a7e49b
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-08-05 20:57:56 +0300

    app/cmd/objectives/app: implements `Reattach` method with support to bubblink merging for overlapping active paths

---
+++ app/cmd/objectives/app/reattach.go
@@ -0,0 +1,119 @@
+package app
+
+import (
+	"context"
+	"fmt"
+	"logbook/cmd/objectives/database"
+	"logbook/models"
+	"logbook/models/columns"
+)
+
+type ReattachParams struct {
+	Actor         columns.UserId
+	CurrentParent models.Ovid
+	NextParent    models.Ovid
+	ComesAfter    columns.ObjectiveId
+	Subject       columns.ObjectiveId
+}
+
+func popCommonActivePath(l, r []models.Ovid) ([]models.Ovid, []models.Ovid, []models.Ovid) {
+	if len(l) == 0 || len(r) == 0 {
+		return l, r, []models.Ovid{}
+	}
+	if len(l) > len(r) {
+		l, r = r, l
+	}
+	c := 0
+	for ; c < len(l) && l[c] == r[c]; c++ {
+		// i like to move it move it
+	}
+	common := l[:c]
+	return l[c:], r[c:], common
+}
+
+// TODO: check auth at the both current and next parent for actor
+func (a *App) Reattach(ctx context.Context, params ReattachParams) error {
+	apCurrent, err := a.ListActivePathToRock(ctx, params.CurrentParent)
+	if err != nil {
+		return fmt.Errorf("checking if the current parent is in active path: %w", err)
+	}
+
+	apNext, err := a.ListActivePathToRock(ctx, params.NextParent)
+	if err != nil {
+		return fmt.Errorf("checking if the next parent is in active path: %w", err)
+	}
+
+	opDetach, err := a.queries.InsertOperation(ctx, database.InsertOperationParams{
+		Subjectoid: params.CurrentParent.Oid,
+		Subjectvid: params.CurrentParent.Vid,
+		Actor:      params.Actor,
+		OpType:     database.OpTypeObjDetach,
+		OpStatus:   database.OpStatusAccepted,
+	})
+	if err != nil {
+		return fmt.Errorf("inserting operation for detaching the objective from current parrent: %w", err)
+	}
+
+	_, err = a.queries.InsertOpObjAttach(ctx, database.InsertOpObjAttachParams{
+		Opid:  opDetach.Opid,
+		Child: params.CurrentParent.Oid,
+	})
+	if err != nil {
+		return fmt.Errorf("inserting operation specific details for detaching the objective from current parent: %w", err)
+	}
+
+	opAttach, err := a.queries.InsertOperation(ctx, database.InsertOperationParams{
+		Subjectoid: params.NextParent.Oid,
+		Subjectvid: params.NextParent.Vid,
+		Actor:      opDetach.Actor,
+		OpType:     database.OpTypeObjAttach,
+		OpStatus:   database.OpStatusAccepted,
+	})
+	if err != nil {
+		return fmt.Errorf("inserting operation for attaching the objective to next parrent: %w", err)
+	}
+
+	_, err = a.queries.InsertOpObjAttach(ctx, database.InsertOpObjAttachParams{
+		Opid:  opAttach.Opid,
+		Child: params.NextParent.Oid,
+	})
+	if err != nil {
+		return fmt.Errorf("inserting operation specific details for attaching the objective to next parent: %w", err)
+	}
+
+	apCurrent, apNext, apCommon := popCommonActivePath(apCurrent, apNext)
+	opidCurrent, err := a.bubblink(ctx, apCurrent, opDetach)
+	if err != nil {
+		return fmt.Errorf("promoting the update to the the current parent is in active path: %w", err)
+	}
+	opidNext, err := a.bubblink(ctx, apNext, opAttach)
+	if err != nil {
+		return fmt.Errorf("promoting the update to the the next parent is in active path: %w", err)
+	}
+	if len(apCommon) > 0 {
+		opMerg, err := a.queries.InsertOperation(ctx, database.InsertOperationParams{
+			Subjectoid: apCommon[len(apCommon)-1].Oid,
+			Subjectvid: apCommon[len(apCommon)-1].Vid,
+			Actor:      columns.ZeroUserId,
+			OpType:     database.OpTypeDoubleTransitiveMerger,
+			OpStatus:   database.OpStatusAccepted,
+		})
+		if err != nil {
+			return fmt.Errorf("inserting operation for merging attachment and detachment operations that crossed on the same ascendant in their bubblinks: %w", err)
+		}
+		_, err = a.queries.InsertOpDoubleTransitiveMerger(ctx, database.InsertOpDoubleTransitiveMergerParams{
+			Opid:   opMerg.Opid,
+			First:  opidCurrent,
+			Second: opidNext,
+		})
+		if err != nil {
+			return fmt.Errorf("inserting operation specific details for merging attachment and detachment operations that crossed on the same ascendant in their bubblinks: %w", err)
+		}
+		_, err = a.bubblink(ctx, apCommon, opAttach)
+		if err != nil {
+			return fmt.Errorf("promoting the update to the overlapping active paths: %w", err)
+		}
+	}
+
+	return nil
+}
---
+++ app/cmd/objectives/app/reattach_test.go
@@ -0,0 +1,47 @@
+package app
+
+import (
+	"logbook/models"
+	"logbook/models/columns"
+	"slices"
+	"testing"
+)
+
+func newOvid() models.Ovid {
+	return models.Ovid{columns.NewUuidV4Unsafe[columns.ObjectiveId](), columns.NewUuidV4Unsafe[columns.VersionId]()}
+}
+
+func TestPopCommonActivePath(t *testing.T) {
+	var (
+		c = []models.Ovid{
+			newOvid(),
+			newOvid(),
+			newOvid(),
+			newOvid(),
+		}
+		l = []models.Ovid{
+			newOvid(),
+			newOvid(),
+			newOvid(),
+			newOvid(),
+		}
+		r = []models.Ovid{
+			newOvid(),
+			newOvid(),
+			newOvid(),
+			newOvid(),
+		}
+	)
+
+	l2, r2, c2 := popCommonActivePath(slices.Concat(c, l), slices.Concat(c, r))
+
+	if !slices.Equal(l, l2) {
+		t.Errorf("assert l = l2\n\tl=%v\n\tl2=%v", l, l2)
+	}
+	if !slices.Equal(r, r2) {
+		t.Errorf("assert r = r2\n\tr=%v\n\tr2=%v", r, r2)
+	}
+	if !slices.Equal(c, c2) {
+		t.Errorf("assert c = c2\n\tc=%v\n\tc2=%v", c, c2)
+	}
+}
