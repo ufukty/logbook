5089ebe2194a6f9400ff5eb53d2865a2ea208cd3
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-08-07 19:46:03 +0300

    app/cmd/objectives/app: implements a testcase following common use case

---
+++ app/cmd/objectives/app/app_test.go
@@ -0,0 +1,89 @@
+package app
+
+import (
+	"context"
+	"fmt"
+	"logbook/cmd/objectives/database"
+	"logbook/cmd/objectives/service"
+	"logbook/models"
+	"logbook/models/columns"
+	"testing"
+)
+
+func TestApp(t *testing.T) {
+	uid, err := columns.NewUuidV4[columns.UserId]()
+	if err != nil {
+		t.Fatal(fmt.Errorf("prep, uid: %w", err))
+	}
+
+	srvcnf, err := service.ReadConfig("../local.yml")
+	if err != nil {
+		t.Fatal(fmt.Errorf("reading service config: %w", err))
+	}
+	err = database.RunMigration(srvcnf)
+	if err != nil {
+		t.Fatal(fmt.Errorf("running migration: %w", err))
+	}
+
+	q, err := database.New(srvcnf.Database.Dsn)
+	if err != nil {
+		t.Fatal(fmt.Errorf("prep, db connect: %w", err))
+	}
+	defer q.Close()
+
+	a := New(q)
+	ctx := context.Background()
+
+	t.Run("rock", func(t *testing.T) {
+		err = a.RockCreate(ctx, uid)
+		if err != nil {
+			t.Fatal(fmt.Errorf("act 1: %w", err))
+		}
+	})
+
+	var rock models.Ovid
+	t.Run("select bookmarks", func(t *testing.T) {
+		bs, err := a.ListBookmarks(ctx, ListBookmarksParams{Viewer: uid})
+		if err != nil {
+			t.Fatal(fmt.Errorf("listing bookmarks: %w", err))
+		}
+
+		found := false
+		for _, b := range bs {
+			if b.IsRock {
+				rock = models.Ovid{b.Oid, b.Vid}
+				found = true
+			}
+		}
+		if !found {
+			t.Fatal(fmt.Errorf("assert, expected rock to be found: %v", bs))
+		}
+	})
+
+	t.Run("create first task", func(t *testing.T) {
+		err := a.CreateSubtask(ctx, CreateSubtaskParams{
+			Creator: uid,
+			Parent:  rock,
+			Content: "Hello world 1",
+		})
+		if err != nil {
+			t.Fatal(fmt.Errorf("CreateSubtask: %w", err))
+		}
+	})
+
+	t.Run("view build", func(t *testing.T) {
+		v, err := a.ViewBuilder(ctx, ViewBuilderParams{
+			Viewer: uid,
+			Root:   rock,
+			Start:  0,
+			Length: 2,
+		})
+		if err != nil {
+			t.Fatal(fmt.Errorf("ViewBuilder: %w", err))
+		}
+
+		for _, e := range v {
+			fmt.Println(e)
+		}
+	})
+}
