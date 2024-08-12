36e9b0ef92f17d23b6c17eb1af449d792e27592e
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-08-12 19:07:08 +0300

    app/cmd/objectives/app: implements `findControlArea`

---
+++ app/cmd/objectives/app/findcontrolarea.go
@@ -0,0 +1,33 @@
+package app
+
+import (
+	"context"
+	"fmt"
+	"logbook/cmd/objectives/database"
+	"logbook/models"
+	"slices"
+
+	"github.com/jackc/pgx/v5"
+)
+
+var ErrControlAreaNotFound = fmt.Errorf("control area not found")
+
+func (a *App) findControlArea(ctx context.Context, subject models.Ovid) (*database.ControlArea, error) {
+	ap, err := a.listActivePathToRock(ctx, subject)
+	if err != nil {
+		return nil, fmt.Errorf("listActivePathToRock: %w", err)
+	}
+	slices.Reverse(ap)
+	var ascendant models.Ovid
+	for _, ascendant = range ap {
+		ca, err := a.queries.SelectControlAreaOnObjective(ctx, ascendant.Oid)
+		if err == pgx.ErrNoRows {
+			continue
+		} else if err != nil {
+			return nil, fmt.Errorf("SelectControlAreaOnObjective: %w", err)
+		} else {
+			return &ca, nil
+		}
+	}
+	return nil, ErrControlAreaNotFound
+}
