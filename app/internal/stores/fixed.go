10c09ffaeb5625160ef597866af7c98f3983e7d2
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-08-23 13:24:08 +0300

    app/internal/stores: implements basic KV store `FixedSizeKV` to be used as the cache for unfolded subtree sizes per user

---
+++ app/internal/stores/fixed.go
@@ -0,0 +1,60 @@
+package stores
+
+import (
+	"logbook/internal/web/logger"
+	"sync"
+)
+
+type FixedSizeKV[K comparable, V any] struct {
+	Size  int
+	store map[K]V
+	mu    sync.RWMutex
+	log   logger.Logger
+}
+
+// TODO: implement LRU cache, if it'll perform better on storing integers (as the timestamps will consume memory too)
+func (f *FixedSizeKV[K, V]) maintainsize() {
+	if len(f.store) < f.Size {
+		return
+	}
+	f.mu.Lock()
+	defer f.mu.Unlock()
+	f.log.Println("reducing size")
+	keys := make([]K, 0, len(f.store))
+	for key := range f.store {
+		keys = append(keys, key)
+	}
+	for _, key := range keys[:int(f.Size/2)] {
+		delete(f.store, key)
+	}
+}
+
+func (f *FixedSizeKV[K, V]) Set(k K, v V) {
+	f.maintainsize()
+	f.mu.Lock()
+	defer f.mu.Unlock()
+	f.store[k] = v
+}
+
+func (f *FixedSizeKV[K, V]) Get(k K) (V, bool) {
+	f.mu.RLock()
+	defer f.mu.RUnlock()
+	v, ok := f.store[k]
+	return v, ok
+}
+
+func (f *FixedSizeKV[K, V]) Delete(k K) {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+	delete(f.store, k)
+}
+
+func (f *FixedSizeKV[K, V]) Keys() []K {
+	f.mu.RLock()
+	defer f.mu.RUnlock()
+	keys := make([]K, 0, len(f.store))
+	for key := range f.store {
+		keys = append(keys, key)
+	}
+	return keys
+}
