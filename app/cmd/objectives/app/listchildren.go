efffca0c730a7948a5e6c1c123cf813b5eeac7dc
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-08-27 20:00:46 +0300

    app/cmd/objectives/app: implements `ListChildren`

---
+++ app/cmd/objectives/app/listchildren.go
@@ -0,0 +1,23 @@
+package app
+
+import (
+	"context"
+	"fmt"
+	"logbook/cmd/objectives/queries"
+	"logbook/models"
+)
+
+func (a *App) ListChildren(ctx context.Context, parent models.Ovid) ([]models.Ovid, error) {
+	subs, err := a.oneshot.SelectSubLinks(ctx, queries.SelectSubLinksParams{
+		SupOid: parent.Oid,
+		SupVid: parent.Vid,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("SelectSubLinks: %w", err)
+	}
+	children := []models.Ovid{}
+	for _, sub := range subs {
+		children = append(children, models.Ovid{Oid: sub.SubOid, Vid: sub.SubVid})
+	}
+	return children, nil
+}
