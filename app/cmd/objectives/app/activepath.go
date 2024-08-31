56ff29d5a91f5cb4b232be55d99a1ca6d1fdad6d
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-08-31 12:20:28 +0300

    app/cmd/objectives/app: modifies code organization without functionality

--- app/cmd/objectives/app/activepath.go
+++ app/cmd/objectives/app/activepath.go
@@ -10,44 +10,43 @@ import (
 
 var ErrLeftBehind = fmt.Errorf("the objective is either directly or eventually linked to an objective which its version is left behind")
 
-// returns a path from the node to the root: [subject, active-ascendants..., rock] or [ErrLeftBehind]
-
-func (a *App) listActivePathToRock(ctx context.Context, q *queries.Queries, subject models.Ovid) ([]models.Ovid, error) {
-	var helper func(ctx context.Context, subject models.Ovid) ([]models.Ovid, error)
-	helper = func(ctx context.Context, subject models.Ovid) ([]models.Ovid, error) {
-		active, err := q.SelectActive(ctx, subject.Oid)
-		if err != nil {
-			return nil, fmt.Errorf("SelectActive(%s): %w", subject.Oid, err)
-		}
-		if subject.Vid != active.Vid {
-			return nil, ErrLeftBehind
-		}
-
-		parents, err := q.SelectUpperLinks(ctx, queries.SelectUpperLinksParams{
-			SubOid: subject.Oid,
-			SubVid: subject.Vid,
-		})
-		if len(parents) == 0 {
-			return []models.Ovid{subject}, nil // assuming it is the rock or an orphaned subtree
-		} else if err != nil {
-			return nil, fmt.Errorf("SelectUpperLinks(%s): %w", subject, err)
-		}
-
-		for _, parent := range parents {
-			path, err := helper(ctx, models.Ovid{parent.SupOid, parent.SupVid})
-			if err == ErrLeftBehind {
-				continue // try other parents
-			} else if err != nil {
-				return nil, fmt.Errorf("ListActivePathToRock(%s): %w", parent, err)
-			} else {
-				return append(path, subject), nil
-			}
-		}
+func listActivePathToRockHelper(ctx context.Context, q *queries.Queries, subject models.Ovid) ([]models.Ovid, error) {
+	active, err := q.SelectActive(ctx, subject.Oid)
+	if err != nil {
+		return nil, fmt.Errorf("SelectActive(%s): %w", subject.Oid, err)
+	}
+	if subject.Vid != active.Vid {
 		return nil, ErrLeftBehind
 	}
-	ap, err := helper(ctx, subject)
+
+	parents, err := q.SelectUpperLinks(ctx, queries.SelectUpperLinksParams{
+		SubOid: subject.Oid,
+		SubVid: subject.Vid,
+	})
+	if len(parents) == 0 {
+		return []models.Ovid{subject}, nil // assuming it is the rock or an orphaned subtree
+	} else if err != nil {
+		return nil, fmt.Errorf("SelectUpperLinks(%s): %w", subject, err)
+	}
+
+	for _, parent := range parents {
+		path, err := listActivePathToRockHelper(ctx, q, models.Ovid{parent.SupOid, parent.SupVid})
+		if err == ErrLeftBehind {
+			continue // try other parents
+		} else if err != nil {
+			return nil, fmt.Errorf("listActivePathToRockHelper(%s): %w", parent, err)
+		} else {
+			return append(path, subject), nil
+		}
+	}
+	return nil, ErrLeftBehind
+}
+
+// returns a path from the node to the root: [subject, active-ascendants..., rock] or [ErrLeftBehind]
+func (a *App) listActivePathToRock(ctx context.Context, q *queries.Queries, subject models.Ovid) ([]models.Ovid, error) {
+	ap, err := listActivePathToRockHelper(ctx, q, subject)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("listActivePathToRockHelper: %w", err)
 	}
 	slices.Reverse(ap)
 	return ap, nil
