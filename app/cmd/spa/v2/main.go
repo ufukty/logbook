065f2ce8ce272624f5ae1085b7bd7ceb71b9aece
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-09-14 07:31:22 +0300

    app/cmd/spa/v2: fixes the server using relative path to locate website files

--- app/cmd/spa/v2/main.go
+++ app/cmd/spa/v2/main.go
@@ -1,12 +1,15 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"os"
+	"path/filepath"
+)
 
 // TODO: Add header: Content-Security-Policy: default-src https://developers.redhat.com
 func main() {
-
-	fs := http.FileServer(http.Dir(""))
+	root := filepath.Join(os.Getenv("WORKSPACE"), "app/cmd/spa/v2")
+	fs := http.FileServer(http.Dir(root))
 	http.Handle("/", http.StripPrefix("/", fs))
-
 	http.ListenAndServe(":80", nil)
 }
