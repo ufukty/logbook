b77a26d2c9c9da30fbdc22ae0c79f30d43be75d8
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-09-15 07:47:59 +0300

    frontend: renames `fe` folder as `backend`

--- frontend/v2/main.go
+++ frontend/v2/main.go
@@ -8,7 +8,7 @@ import (
 
 // TODO: Add header: Content-Security-Policy: default-src https://developers.redhat.com
 func main() {
-	root := filepath.Join(os.Getenv("WORKSPACE"), "app/cmd/spa/v2")
+	root := filepath.Join(os.Getenv("WORKSPACE"), "frontend/cmd/spa/v2")
 	fs := http.FileServer(http.Dir(root))
 	http.Handle("/", http.StripPrefix("/", fs))
 	http.ListenAndServe(":80", nil)
