6146037986b5aa6dcb622bb67497526ed43f0906
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-09-16 09:57:08 +0300

    be/cmd/api: fixes `api-gateway` failing at locating service instances through `sidecar` due the sidecar gets initialized and deferred to stop inside a separate function rather than the `main`. which leads the sidecar to stop early

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -3,24 +3,40 @@ package main
 import (
 	"fmt"
 	"log"
-	"logbook/cmd/api/forwarders"
+	registry "logbook/cmd/registry/client"
+	"logbook/config/api"
 	"logbook/internal/startup"
+	"logbook/internal/web/balancer"
+	"logbook/internal/web/forwarder"
+	"logbook/internal/web/registryfile"
 	"logbook/internal/web/router"
+	"logbook/internal/web/sidecar"
+	"logbook/models"
 
 	"github.com/gorilla/mux"
 )
 
-func mainerr() error {
+func Main() error {
 	args, deplcfg, apicfg, err := startup.ApiGateway()
 	if err != nil {
 		return fmt.Errorf("reading configs: %w", err)
 	}
 
-	fws, err := forwarders.New(args, deplcfg, apicfg)
-	if err != nil {
-		return fmt.Errorf("forwarders.New: %w", err)
-	}
-	defer fws.Stop()
+	internalsd := registryfile.NewFileReader(args.InternalGateway, deplcfg.ServiceDiscovery.UpdatePeriod, registryfile.ServiceParams{
+		Port: deplcfg.Ports.Internal,
+		Tls:  true,
+	})
+	// NOTE: service registry needs to be accessed through internal gateway
+	sc := sidecar.New(registry.NewClient(balancer.New(internalsd), apicfg, true), deplcfg.ServiceDiscovery.UpdatePeriod, []models.Service{
+		models.Account,
+		models.Objectives,
+	})
+	defer sc.Stop()
+	defer internalsd.Stop()
+	defer sc.Stop()
+
+	accounts := forwarder.New(sc.InstanceSource(models.Account), models.Account, api.PathFromInternet(apicfg.Public.Services.Account))
+	objectives := forwarder.New(sc.InstanceSource(models.Objectives), models.Objectives, api.PathFromInternet(apicfg.Public.Services.Objectives))
 
 	router.StartServer(router.ServerParameters{
 		Router: deplcfg.Router,
@@ -30,15 +46,15 @@ func mainerr() error {
 	}, func(r *mux.Router) {
 		r = r.UseEncodedPath()
 		sub := r.PathPrefix(apicfg.Public.Path).Subrouter()
-		sub.PathPrefix(apicfg.Public.Services.Account.Path).HandlerFunc(fws.Accounts.Handler)
-		sub.PathPrefix(apicfg.Public.Services.Objectives.Path).HandlerFunc(fws.Objectives.Handler)
+		sub.PathPrefix(apicfg.Public.Services.Account.Path).HandlerFunc(accounts.Handler)
+		sub.PathPrefix(apicfg.Public.Services.Objectives.Path).HandlerFunc(objectives.Handler)
 	})
 
 	return nil
 }
 
 func main() {
-	if err := mainerr(); err != nil {
+	if err := Main(); err != nil {
 		log.Fatalln(err)
 	}
 }
