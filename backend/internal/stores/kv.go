4f82082dbf32e79fd7371b54cfb96b168d1af24f
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-09-18 11:54:54 +0300

    be/int/stores: adds a basic KV store called `KV` which doesn't contain a size limitation like `FixedSizeKV` does. this one is supposed to be used as a temporary solution where concurrent maps are needed

---
+++ backend/internal/stores/kv.go
@@ -0,0 +1,55 @@
+package stores
+
+import (
+	"logbook/internal/web/logger"
+	"sync"
+)
+
+type KV[K comparable, V any] struct {
+	store map[K]V
+	mu    sync.RWMutex
+	log   logger.Logger
+}
+
+var _ Stores[any, any] = &KV[any, any]{}
+
+func NewKV[K comparable, V any](logname string) *KV[K, V] {
+	return &KV[K, V]{
+		store: map[K]V{},
+		mu:    sync.RWMutex{},
+		log:   *logger.NewLogger(logname),
+	}
+}
+
+func (f *KV[K, V]) Len(k K, v V) int {
+	return len(f.store)
+}
+
+func (f *KV[K, V]) Set(k K, v V) {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+	f.store[k] = v
+}
+
+func (f *KV[K, V]) Get(k K) (V, bool) {
+	f.mu.RLock()
+	defer f.mu.RUnlock()
+	v, ok := f.store[k]
+	return v, ok
+}
+
+func (f *KV[K, V]) Delete(k K) {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+	delete(f.store, k)
+}
+
+func (f *KV[K, V]) Keys() []K {
+	f.mu.RLock()
+	defer f.mu.RUnlock()
+	keys := make([]K, 0, len(f.store))
+	for key := range f.store {
+		keys = append(keys, key)
+	}
+	return keys
+}
