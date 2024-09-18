adafea2d0fb176d3a3397d0d788008ebe62ea3c4
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-09-18 07:17:42 +0300

    be/test.sh: switches to utility provided in autosource and removes the content `run.sh` also contains

--- backend/test.sh
+++ backend/test.sh
@@ -2,93 +2,13 @@
 set -e -E
 set -o pipefail
 
-# to clean before & after
-ports=({8080..8082} {8090..8091})
+for port in {8080..8082}; do
+  until curl --fail "https://localhost:${port}/ping" >/dev/null 2>&1; do sleep 1; done
+done
 
-function nextcolor() {
-  echo $(($(jobs -p | wc -l) % 6 + 31))
-}
-
-function prefix() {
-  COLOR="${1:?}"
-  shift 1
-  PREFIX="$(echo "$@" | tr -d ';')"
-  esc=$(printf '\033')
-  total_length=20
-  space_count=$((total_length - ${#PREFIX}))
-  padding=$(printf "%${space_count}s")
-  "$@" | gsed -E "s;^(.*)$;${esc}\[${COLOR}m$PREFIX${padding}:${esc}\[0m \1;g"
-}
-
-function cleanports() {
-  for port in "${ports[@]}"; do
-    lsof -i ":$port" >/dev/null 2>&1 && kill -9 "$(lsof -i ":$port" | tail -n 1 | cut -d ' ' -f 2)" >/dev/null 2>&1
-  done
-  return 0
-}
-
-function pings() {
-  for port in "${ports[@]}"; do
-    until curl --fail "https://localhost:${port}/ping" >/dev/null 2>&1; do sleep 1; done
-  done
-}
-
-function service() {
-  SERVICENAME="${1:?}"
-  if test -d "cmd/${SERVICENAME}/database"; then
-    /usr/local/go/bin/go test -timeout 10s -run '^TestMigration$' "logbook/cmd/${SERVICENAME}/database" -v -count=1
-  fi
-  unbuffer go run "logbook/cmd/${SERVICENAME}" \
-    -e local \
-    -api api.yml \
-    -service "cmd/${SERVICENAME}/local.yml" \
-    -deployment "../platform/local/deployment.yml" \
-    -internal "../platform/local/registryfile.internalgateway.json" \
-    -cert "../platform/local/tls/localhost.crt" \
-    -key "../platform/local/tls/localhost.key"
-}
-
-function registry() {
-  unbuffer go run "logbook/cmd/registry" \
-    -e local \
-    -api api.yml \
-    -deployment "../platform/local/deployment.yml" \
-    -cert "../platform/local/tls/localhost.crt" \
-    -key "../platform/local/tls/localhost.key"
-}
-
-function api-gateway() {
-  unbuffer go run "logbook/cmd/api" \
-    -e local \
-    -api api.yml \
-    -deployment "../platform/local/deployment.yml" \
-    -internal "../platform/local/registryfile.internalgateway.json" \
-    -cert "../platform/local/tls/localhost.crt" \
-    -key "../platform/local/tls/localhost.key"
-}
-
-function internal-gateway() {
-  unbuffer go run "logbook/cmd/internal" \
-    -e local \
-    -api api.yml \
-    -deployment "../platform/local/deployment.yml" \
-    -registy "../platform/local/registryfile.registryservice.json" \
-    -cert "../platform/local/tls/localhost.crt" \
-    -key "../platform/local/tls/localhost.key"
-}
-
-trap cleanports EXIT
-cleanports
-
-prefix "$(nextcolor)" registry &
-prefix "$(nextcolor)" service account &
-prefix "$(nextcolor)" service objectives &
-prefix "$(nextcolor)" api-gateway &
-prefix "$(nextcolor)" internal-gateway &
-
-pings
 set +e
 
-prefix "$(nextcolor)" unbuffer httpyac tests/api/discovery.http --all --insecure
-prefix "$(nextcolor)" unbuffer httpyac tests/api/accounts.http --all --insecure
-prefix "$(nextcolor)" unbuffer httpyac tests/api/objectives.http --all --insecure
+echo "starting tests..." | prefix "$YELLOW" test
+unbuffer httpyac tests/api/discovery.http --all --insecure 2>&1 | prefix "$YELLOW" test discovery
+unbuffer httpyac tests/api/accounts.http --all --insecure 2>&1 | prefix "$YELLOW" test accounts
+unbuffer httpyac tests/api/objectives.http --all --insecure 2>&1 | prefix "$YELLOW" test objectives
