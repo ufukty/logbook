3ae0c3daf44799db340cbd6efa7b90a93da8a593
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-10-06 16:22:53 +0300

    be/int/logger: changes the signature of `Printf` and `Fatalf`

--- backend/internal/logger/logger.go
+++ backend/internal/logger/logger.go
@@ -47,12 +47,8 @@ func (l Logger) Println(args ...any) {
 	log.Println(append([]any{l.name}, args...)...)
 }
 
-func (l Logger) Printf(args ...any) {
-	if format, ok := args[0].(string); ok {
-		log.Printf("%s "+format, append([]any{l.name}, args[1:]...)...)
-	} else {
-		panic("First argument Logger.Printf call should be the format string")
-	}
+func (l Logger) Printf(format string, args ...any) {
+	log.Printf("%s "+format, append([]any{l.name}, args...)...)
 }
 
 func (l Logger) Fatal(args ...any) {
@@ -63,10 +59,6 @@ func (l Logger) Fatalln(args ...any) {
 	log.Fatalln(append([]any{l.name}, args...)...)
 }
 
-func (l Logger) Fatalf(args ...any) {
-	if format, ok := args[0].(string); ok {
-		log.Fatalf("%s "+format, append([]any{l.name}, args[1:]...)...)
-	} else {
-		panic("First argument Logger.Fatalf call should be the format string")
-	}
+func (l Logger) Fatalf(format string, args ...any) {
+	log.Fatalf("%s "+format, append([]any{l.name}, args...)...)
 }
