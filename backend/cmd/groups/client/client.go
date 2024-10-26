2bc931589581c4857acb66ff90ae28254558a966
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-10-26 22:43:14 +0300

    be/cmd/groups: adds client method for membership check endpoint

--- backend/cmd/groups/client/client.go
+++ backend/cmd/groups/client/client.go
@@ -1,8 +1,10 @@
 package objectives
 
 import (
+	"logbook/cmd/groups/endpoints/private"
 	"logbook/config/api"
 	"logbook/internal/web/balancer"
+	"logbook/internal/web/requests"
 )
 
 type Client struct {
@@ -15,4 +17,8 @@ func NewClient(lb *balancer.LoadBalancer, apicfg *api.Config) *Client {
 		servicecfg: apicfg.Groups,
 		lb:         lb,
 	}
+}
+
+func (c *Client) MembershipCheck(bq *private.MembershipCheckRequest) (*private.MembershipCheckResponse, error) {
+	return requests.BalancedSend(c.lb, "", c.servicecfg.Private.MembershipCheck, bq, &private.MembershipCheckResponse{})
 }
