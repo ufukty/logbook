97cf503eeeb97e35cacf99f14eb9a881c889c68d
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-10-31 23:12:30 +0300

    be/models/columns: adopts govalid for `LinkType`

---
+++ backend/models/columns/enums.go
@@ -0,0 +1,7 @@
+package columns
+
+const (
+	Primary = LinkType("PRIMARY") // eg. When task owner break downs it
+	Private = LinkType("PRIVATE") //
+	Remote  = LinkType("REMOTE")  // eg. Collaborated objective attached to local objectives
+)
--- backend/models/columns/types.go
+++ backend/models/columns/types.go
@@ -45,9 +45,3 @@ type (
 	LinkType string
 	TagId    string
 )
-
-const (
-	Primary = LinkType("PRIMARY") // eg. When task owner break downs it
-	Private = LinkType("PRIVATE") //
-	Remote  = LinkType("REMOTE")  // eg. Collaborated objective attached to local objectives
-)
