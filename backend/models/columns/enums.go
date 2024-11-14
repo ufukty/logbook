6e24f4ea658b3855e519ebf90a1fc057af048fb2
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-11-14 22:42:32 +0300

    be/models/columns: renames a file

--- backend/models/columns/enums.go
+++
@@ -1,7 +0,0 @@
-package columns
-
-const (
-	Primary = LinkType("PRIMARY") // eg. When task owner break downs it
-	Private = LinkType("PRIVATE") //
-	Remote  = LinkType("REMOTE")  // eg. Collaborated objective attached to local objectives
-)
