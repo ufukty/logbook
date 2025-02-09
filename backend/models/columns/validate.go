226b4720e0d9fa8adf817dd9fd5e4a43beecc73f
Ufuk <13807261+ufukty@users.noreply.github.com>
2025-02-09 17:21:40 +0300

    be/models/columns: improves readability and consistency in validation and rule definition

--- backend/models/columns/validate.go
+++ backend/models/columns/validate.go
@@ -42,102 +42,28 @@ var (
 	uuid_        = pmm{r(`^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$`), len("00000000-0000-0000-0000-000000000000"), len("00000000-0000-0000-0000-000000000000")}
 )
 
-func (v AccessId) Validate() error {
-	return uuid_.Validate(string(v))
-}
-
-func (v BookmarkId) Validate() error {
-	return uuid_.Validate(string(v))
-}
-
-func (v BottomUpPropsId) Validate() error {
-	return uuid_.Validate(string(v))
-}
-
-func (v CollaborationId) Validate() error {
-	return uuid_.Validate(string(v))
-}
-
-func (v CollaboratorId) Validate() error {
-	return uuid_.Validate(string(v))
-}
-
-func (v ControlAreaId) Validate() error {
-	return uuid_.Validate(string(v))
-}
-
-func (v DelegationId) Validate() error {
-	return uuid_.Validate(string(v))
-}
-
-func (v Email) Validate() error {
-	return email.Validate(string(v))
-}
-
-func (v GroupId) Validate() error {
-	return uuid_.Validate(string(v))
-}
-
-func (v GroupInviteId) Validate() error {
-	return uuid_.Validate(string(v))
-}
-
-func (v GroupMembershipId) Validate() error {
-	return groupTitle.Validate(string(v))
-}
-
-func (v GroupName) Validate() error {
-	return groupTitle.Validate(string(v))
-}
-
-func (v HumanName) Validate() error {
-	return humanName.Validate(string(v))
-}
-
-func (v LinkId) Validate() error {
-	return uuid_.Validate(string(v))
-}
-
-func (v LoginId) Validate() error {
-	return uuid_.Validate(string(v))
-}
-
-func (v ObjectiveId) Validate() error {
-	return uuid_.Validate(string(v))
-}
-
-func (v OperationId) Validate() error {
-	return uuid_.Validate(string(v))
-}
-
-func (v Phone) Validate() error {
-	return phoneNumber.Validate(string(v))
-}
-
-func (v PropertiesId) Validate() error {
-	return uuid_.Validate(string(v))
-}
-
-func (v SessionId) Validate() error {
-	return uuid_.Validate(string(v))
-}
-
-func (v SessionToken) Validate() error {
-	return sessionToken.Validate(string(v))
-}
-
-func (v TagId) Validate() error {
-	return uuid_.Validate(string(v))
-}
-
-func (v UserId) Validate() error {
-	return uuid_.Validate(string(v))
-}
-
-func (v Username) Validate() error {
-	return username.Validate(string(v))
-}
-
-func (v VersionId) Validate() error {
-	return uuid_.Validate(string(v))
-}
+func (v AccessId) Validate() error          { return uuid_.Validate(string(v)) }
+func (v BookmarkId) Validate() error        { return uuid_.Validate(string(v)) }
+func (v BottomUpPropsId) Validate() error   { return uuid_.Validate(string(v)) }
+func (v CollaborationId) Validate() error   { return uuid_.Validate(string(v)) }
+func (v CollaboratorId) Validate() error    { return uuid_.Validate(string(v)) }
+func (v ControlAreaId) Validate() error     { return uuid_.Validate(string(v)) }
+func (v DelegationId) Validate() error      { return uuid_.Validate(string(v)) }
+func (v Email) Validate() error             { return email.Validate(string(v)) }
+func (v GroupId) Validate() error           { return uuid_.Validate(string(v)) }
+func (v GroupInviteId) Validate() error     { return uuid_.Validate(string(v)) }
+func (v GroupMembershipId) Validate() error { return groupTitle.Validate(string(v)) }
+func (v GroupName) Validate() error         { return groupTitle.Validate(string(v)) }
+func (v HumanName) Validate() error         { return humanName.Validate(string(v)) }
+func (v LinkId) Validate() error            { return uuid_.Validate(string(v)) }
+func (v LoginId) Validate() error           { return uuid_.Validate(string(v)) }
+func (v ObjectiveId) Validate() error       { return uuid_.Validate(string(v)) }
+func (v OperationId) Validate() error       { return uuid_.Validate(string(v)) }
+func (v Phone) Validate() error             { return phoneNumber.Validate(string(v)) }
+func (v PropertiesId) Validate() error      { return uuid_.Validate(string(v)) }
+func (v SessionId) Validate() error         { return uuid_.Validate(string(v)) }
+func (v SessionToken) Validate() error      { return sessionToken.Validate(string(v)) }
+func (v TagId) Validate() error             { return uuid_.Validate(string(v)) }
+func (v UserId) Validate() error            { return uuid_.Validate(string(v)) }
+func (v Username) Validate() error          { return username.Validate(string(v)) }
+func (v VersionId) Validate() error         { return uuid_.Validate(string(v)) }
