8166cf0960b6be96e9b58125340ce574c5990b0f
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-05-05 10:54:48 +0300

    be/int/lec: inits waiting changes

---
+++ backend/internal/lec/fixed.go
@@ -0,0 +1,167 @@
+package lec
+
+import (
+	"bytes"
+	"fmt"
+	"math"
+	"slices"
+	"time"
+)
+
+type Fixed struct {
+	start       time.Time
+	length, res time.Duration // TODO: Make another version with fixed length, new data overwritting old data
+	levels      [][]int
+}
+
+func NewFixed(start time.Time, length, resolution time.Duration) *Fixed {
+	i := &Fixed{
+		start:  start,
+		length: length,
+		res:    resolution,
+		levels: [][]int{},
+	}
+	n := 1
+	for cellres := resolution; cellres < length*2; cellres *= 2 {
+		i.levels = append(i.levels, make([]int, n))
+		n *= 2
+	}
+	slices.Reverse(i.levels)
+	return i
+}
+
+func (c *Fixed) Save(t time.Time, v int) {
+	cell := int(float64(t.Sub(c.start).Nanoseconds()) / float64(c.res)) // virtual index in first layer
+	for level := 0; level < len(c.levels); level++ {
+		if cell%2 == 0 { // stored cell
+			c.levels[level][cell/2] += v
+		}
+		cell /= 2 // floor
+	}
+}
+
+func (c *Fixed) cellvalue(level, cell int) int {
+	if len(c.levels) <= level {
+		return 0
+	}
+	if cell%2 == 0 {
+		return c.levels[level][cell/2] // virtual index -> array index
+	} else if level != len(c.levels)-1 {
+		parent := c.cellvalue(level+1, cell/2) // floor
+		sibling := c.cellvalue(level, cell-1)
+		return parent - sibling
+	}
+	return 0
+}
+
+func (c *Fixed) query(from, to, level int) (int, error) {
+	if from > to {
+		return 0, fmt.Errorf("'from' must be less than or equal to 'to'")
+	}
+	total := 0
+	res := pow(2, level)
+	l := int(math.Ceil(float64(from) / float64(res)))
+	r := int(math.Floor(float64(to) / float64(res)))
+	if r < l {
+		if level == 0 {
+			return -1, fmt.Errorf("bad developer") // FIXME:
+		}
+		q, err := c.query(from, to, level-1)
+		if err != nil {
+			return -1, fmt.Errorf("c.query on lower level: %w", err)
+		}
+		return q, nil
+	}
+	for i := l; i < r; i++ {
+		total += c.cellvalue(level, i)
+	}
+	lx := l * res
+	rx := r * res
+	if from != lx {
+		if level == 0 {
+			return -1, fmt.Errorf("bad developer") // FIXME: there should not be excess area resolved beneath first layer
+		}
+		q, err := c.query(from, lx, level-1)
+		if err != nil {
+			return -1, fmt.Errorf("c.query on left: %w", err)
+		}
+		total += q
+	}
+	if to != rx {
+		if level == 0 {
+			return -1, fmt.Errorf("bad developer") // FIXME:
+		}
+		q, err := c.query(rx, to, level-1)
+		if err != nil {
+			return -1, fmt.Errorf("c.query on right: %w", err)
+		}
+		total += q
+	}
+	return total, nil
+}
+
+func (c *Fixed) Query(from, to time.Time) (int, error) {
+	end := c.start.Add(c.length)
+	if from == to {
+		return -1, fmt.Errorf("values 'from' and 'to' are same")
+	}
+	if to.Before(c.start) {
+		return -1, fmt.Errorf("period of query ends before stored period starts: %s", c.start)
+	}
+	if from.After(end) {
+		return -1, fmt.Errorf("period of query starts after stored period ends: %s", end)
+	}
+	if from.Before(c.start) {
+		return -1, fmt.Errorf("period of query starts before stored period starts: %s", c.start)
+	}
+	if to.After(end) {
+		return -1, fmt.Errorf("period of query ends after stored period ends: %s", end)
+	}
+	if from.Sub(c.start).Nanoseconds()%c.res.Nanoseconds() != 0 {
+		return -1, fmt.Errorf("value 'from' doesn't align with resolution")
+	} else if to.Sub(c.start).Nanoseconds()%c.res.Nanoseconds() != 0 {
+		return -1, fmt.Errorf("value 'to' doesn't align with resolution")
+	}
+
+	return c.query(
+		int(from.Sub(c.start).Nanoseconds()/c.res.Nanoseconds()),
+		int(to.Sub(c.start).Nanoseconds()/c.res.Nanoseconds()),
+		len(c.levels)-1,
+	)
+}
+
+func (c *Fixed) dump() string {
+	b := bytes.NewBuffer([]byte{})
+	g := -1
+	for i := 0; i < len(c.levels); i++ {
+		lastci := len(c.levels[i]) - 1
+		lastreal := pow(2, i) * (2*lastci + 1)
+		if g < lastreal {
+			g = lastreal
+		}
+	}
+	d := twodim(len(c.levels), g+1, -1)
+
+	for level := len(c.levels) - 1; level >= 0; level-- {
+		for ci := 0; ci < len(c.levels[level]); ci++ {
+			d[level][(2*ci)*pow(2, level)] = c.cellvalue(level, ci*2)
+			if level != len(c.levels)-1 {
+				d[level][(2*ci+1)*pow(2, level)] = c.cellvalue(level, ci*2+1)
+			}
+		}
+	}
+	d = transpose(d)
+	for i := 0; i < len(d); i++ {
+		if i != 0 {
+			fmt.Fprintln(b)
+		}
+		for j := len(d[i]) - 1; j >= 0; j-- {
+			if d[i][j] != -1 {
+				fmt.Fprintf(b, "%-5d", d[i][j])
+			} else {
+				fmt.Fprintf(b, "     ")
+			}
+		}
+	}
+	return b.String()
+}
---
+++ backend/internal/lec/fixed_test.go
@@ -0,0 +1,96 @@
+package lec
+
+import (
+	"fmt"
+	"logbook/internal/average"
+	"testing"
+	"time"
+)
+
+func TestFixed(t *testing.T) {
+	// Use either read() or generateTestData to prepare timestamps
+	ts, err := read()
+	if err != nil {
+		t.Fatal(fmt.Errorf("prep: %w", err))
+	}
+
+	var (
+		from = ts[0].Truncate(average.Day)
+		to   = ts[len(ts)-1].Truncate(average.Day).Add(average.Day)
+	)
+
+	fmt.Printf("testdata: (%s -> %s)\n", ts[0], ts[len(ts)-1])
+	fmt.Printf("testdata: (%s -> %s) truncated\n", from, to)
+
+	realdiff := ts[len(ts)-1].Sub(ts[0])
+	truncateddiff := to.Sub(from)
+	fmt.Printf("(real period: %s [%f years]) (Δtruncated: %s [%f years]) (diff: %s)\n",
+		realdiff, float64(realdiff)/float64(average.Year),
+		truncateddiff, float64(truncateddiff)/float64(average.Year),
+		time.Duration(truncateddiff-realdiff).Abs(),
+	)
+
+	inf := NewFixed(from, to.Sub(from), average.Day)
+	for _, t := range ts {
+		inf.Save(t, 1)
+	}
+
+	err = dump(inf)
+	if err != nil {
+		t.Fatal(fmt.Errorf("dump: %w", err))
+	}
+
+	type (
+		input struct {
+			from, to time.Time
+		}
+		output struct {
+			q   int
+			err bool
+		}
+		tc struct {
+			name string
+			input
+			output
+		}
+	)
+	tcs := []tc{
+		{"Empty range", input{from, from}, output{err: true}},
+
+		{"Overflow range", input{from, to.Add(average.Day * 10)}, output{err: true}},
+		{"Out of bound (1 day after)", input{to.Add(average.Day), to.Add(average.Day * 2)}, output{err: true}},
+		{"Out of bound (2 days after)", input{to.Add(average.Day * 2), to.Add(average.Day * 3)}, output{err: true}},
+		{"Out of bound (1 week after)", input{to.Add(average.Week), to.Add(average.Week * 2)}, output{err: true}},
+		{"Out of bound (1 week before)", input{from.Add(-3 * average.Week), from.Add(-2 * average.Week)}, output{err: true}},
+
+		{"Full range (adjusted)", input{from, to}, output{q: 1000}},
+		{"Full range (year)", input{from, from.Add(average.Year + average.Day)}, output{q: 1000}},
+
+		{"Jun, 1st", input{from, from.Add(average.Day)}, output{q: 3}},
+		{"Feb, 1st", input{parse(t, "2024-02-01T00:00:00+00:00"), parse(t, "2024-02-02T00:00:00+00:00")}, output{q: 3}},
+		{"Feb, 1st-30th", input{parse(t, "2024-02-01T00:00:00+00:00"), parse(t, "2024-03-01T00:00:00+00:00")}, output{q: 85}},
+		{"Mar, 1st-31th", input{parse(t, "2024-03-01T00:00:00+00:00"), parse(t, "2024-04-01T00:00:00+00:00")}, output{q: 72}},
+		{"Feb, 1st to Mar, 31th", input{parse(t, "2024-02-01T00:00:00+00:00"), parse(t, "2024-04-01T00:00:00+00:00")}, output{q: 157}},
+		{"July, 2nd", input{parse(t, "2024-07-02T00:00:00+00:00"), parse(t, "2024-07-03T00:00:00+00:00")}, output{q: 6}},
+		{"July, 4th", input{parse(t, "2024-07-04T00:00:00+00:00"), parse(t, "2024-07-05T00:00:00+00:00")}, output{q: 3}},
+		{"July, 2nd-5th", input{parse(t, "2024-07-02T00:00:00+00:00"), parse(t, "2024-07-05T00:00:00+00:00")}, output{q: 10}},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			q, err := inf.Query(tc.input.from, tc.input.to)
+			if tc.output.err {
+				if err == nil {
+					t.Fatalf("act, expected error. got '%d'", q)
+				}
+				return
+			}
+			if err != nil {
+				t.Fatalf("act: %v", err)
+			}
+			if q != tc.output.q {
+				t.Errorf("assert, expected %d, got %d", tc.output.q, q)
+			}
+		})
+	}
+}
