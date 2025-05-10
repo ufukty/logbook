083b7a545834d5fcf59273db19666d91d79725c9
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-05-10 16:00:48 +0300

    dev/macos: changes python venv path to in-repository

--- dev/macos.sh
+++ dev/macos.sh
@@ -6,7 +6,9 @@ test -f "$HOME/venv/bin/activate" ||
   python3 -m venv "$HOME/venv"
 source "$HOME/venv/bin/activate"
 
-: "${WORKSPACE:?}"
+test -f ".venv/bin/activate" ||
+  python3 -m venv ".venv"
+source ".venv/bin/activate"
 
 which make ||
   xcode-select --install
