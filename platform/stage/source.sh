d2fbcfce86a3e037ef4c571dd75b88feed3f2fd7
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-05-14 13:59:36 +0300

    platform/stage/source: fixes the ssh-add -l check using wrong value to check existing records

--- platform/stage/source.sh
+++ platform/stage/source.sh
@@ -7,7 +7,7 @@
 : "${TF_VAR_OVPN_AUTH_TOTP:?}"
 : "${TF_VAR_OVPN_AUTH_USERNAME:?}"
 
-(ssh-add -l | grep "do" >/dev/null) ||
+(ssh-add -l | grep -q "$(ssh-keygen -lf secrets/ssh/do.pub)") ||
   (echo "calling ssh-agent" && ssh-agent && ssh-add secrets/ssh/do)
 
 export STAGE="${WORKSPACE:?}/platform/stage"
