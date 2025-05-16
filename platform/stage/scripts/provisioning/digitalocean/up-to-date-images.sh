c4c1cb6cec61ca94243d312562a1cd44492d818a
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-05-16 19:59:44 +0300

    platform/stage/scripts/provisioning/digitalocean: deletes unused scripts

--- platform/stage/scripts/provisioning/digitalocean/up-to-date-images.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/local/bin/bash
-
-ALL="$(doctl compute snapshot list --format Name,ID --no-header | grep build | grep -v base | sort -r | gsed -E 's/[ ]+/ /g')"
-echo "$ALL" | gsed -E 's/(build_([^_]+)_([0-9_]{17}).*)/\1 \2/g' | sort -r | uniq -f 2 | cut -d ' ' -f 1-2 | gsed -E 's/^([^\ ]+) (.*)/\2 \1/g'
