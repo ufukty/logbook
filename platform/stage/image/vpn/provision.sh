e021211f48799d5df110ccf4eb5c10ed6a808980
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-05-19 17:48:35 +0300

    platform/stage/image/vpn: updates writing style of `provision.sh`. removes the install of easyrsa, as the change in design to deploy locally generated keys to server in provision, instead of generating them in server

--- platform/stage/image/vpn/provision.sh
+++ platform/stage/image/vpn/provision.sh
@@ -1,88 +1,49 @@
 #!/bin/bash
 
+PS4='\033[33m''\D{%H:%M:%S} provision:${LINENO}:''\033[0m '
+set -xe
+
 # ---------------------------------------------------------------------------- #
-# Commons
+# Wait
 # ---------------------------------------------------------------------------- #
 
-PS4="\033[32m/\$(basename \"\${BASH_SOURCE}\"):\${LINENO}\033[0m\033[33m\${FUNCNAME[0]:+/\${FUNCNAME[0]}():}\033[0m "
-set -x
-set -v
-set -e
-set -E
+timeout 180 bash -c "until stat /var/lib/cloud/instance/boot-finished 2>/dev/null; do sleep 2; done"
 
-PROVISIONER_FILES="$(pwd -P)"
+# ---------------------------------------------------------------------------- #
+# Pre checks
+# ---------------------------------------------------------------------------- #
 
-function retry() {
-    local COUNTER=0
-    until "$@"; do
-        EC=$?
-        COUNTER=$((COUNTER + 1))
-        test $COUNTER -ge 60 && exit $EC
-        sleep 10
-    done
-}
+test $EUID -eq 0 # assert_sudo
+systemctl restart systemd-journald
+test -e /dev/net/tun # check_tun_availability
+cloud-init status --wait >/dev/null
 
-function apt_update() { retry apt-get update; }
-function restart_journald() { systemctl restart systemd-journald; }
-function assert_sudo() { test $EUID -eq 0; }
-function remove_password_change_requirement() { sed --in-place -E 's/root:(.*):0:0:(.*):/root:\1:18770:0:\2:/g' /etc/shadow; }
-function wait_cloud_init() { cloud-init status --wait >/dev/null; }
-function check_tun_availability() { test -e /dev/net/tun; }
-function deploy_provisioner_files() {
-    chmod 700 -R "$PROVISIONER_FILES/map"
-    chown root:root -R "$PROVISIONER_FILES/map"
-    rsync --verbose --recursive "$PROVISIONER_FILES/map/" "/"
-    rm -rfv "$PROVISIONER_FILES/map"
-}
+# ---------------------------------------------------------------------------- #
+# Action
+# ---------------------------------------------------------------------------- #
 
 export DEBIAN_FRONTEND=noninteractive
+apt-get update -y
+apt-get install -y ca-certificates curl gnupg iptables openssl openvpn unbound wget
+
+test -d /etc/openvpn/easy-rsa && rm -rfv /etc/openvpn/easy-rsa/*
+
+# ovpn_auth
+chmod 755 /etc/openvpn/ovpn-auth-v1.0.7-linux-x64
+chown root:root /etc/openvpn/ovpn-auth-v1.0.7-linux-x64
+
+chmod 744 /etc/openvpn/ovpn_auth_database.yml
+chown root:root /etc/openvpn/ovpn_auth_database.yml
 
 # ---------------------------------------------------------------------------- #
-# Definitions
-# ---------------------------------------------------------------------------- #-
-
-function install_utilities() {
-    retry apt-get install -y ipcalc
-}
-
-function install_openvpn() {
-    retry apt-get install -y ca-certificates gnupg openvpn iptables openssl wget ca-certificates curl
-    test -d /etc/openvpn/easy-rsa && rm -rf /etc/openvpn/easy-rsa/*
-    return "0"
-}
-
-function install_easy_rsa() (
-    cd "$PROVISIONER_FILES/dependencies"
-    mkdir -p /etc/openvpn/easy-rsa
-    tar xzf EasyRSA-3.1.7.tgz --strip-components=1 --directory /etc/openvpn/easy-rsa
-)
-
-function ovpn_auth() {
-    chmod 755 /etc/openvpn/ovpn-auth-v1.0.7-linux-x64
-    chown root:root /etc/openvpn/ovpn-auth-v1.0.7-linux-x64
-
-    chmod 744 /etc/openvpn/ovpn_auth_database.yml
-    chown root:root /etc/openvpn/ovpn_auth_database.yml
-}
-
-function install_unbound() {
-    retry apt-get install -y unbound
-}
-
-# ---------------------------------------------------------------------------- #
-# Main
+# Mapping
 # ---------------------------------------------------------------------------- #
 
-assert_sudo
-restart_journald
-check_tun_availability
-wait_cloud_init
-apt_update
+find map -type f | while read FILE; do
+  sudo mkdir -pv "$(dirname "${FILE/map/}")"
+  sudo mv -v "${FILE}" "${FILE/map/}"
+done
+rm -rfv /root/map
 
-install_utilities
-install_openvpn
-install_easy_rsa
-install_unbound
-
-deploy_provisioner_files
-ovpn_auth
+# remove_password_change_requirement
+# sed --in-place -E 's/root:(.*):0:0:(.*):/root:\1:18770:0:\2:/g' /etc/shadow;
