62862c658b58af8a2f1ae08c5e8037d4db63c883
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-05-27 23:40:00 +0300

    platform/stage/scripts/deployment/vpn-clients: fixes path args

--- platform/stage/scripts/deployment/vpn-clients.sh
+++ platform/stage/scripts/deployment/vpn-clients.sh
@@ -5,13 +5,8 @@ set -xe
 
 : "${VPS_SUDO_USER:?}"
 
-test -f "$STAGE/provision/vpn/terraform.tfstate" && (
-
-  # ---------------------------------------------------------------------------- #
-  # DO regions
-  # ---------------------------------------------------------------------------- #
-
-  jq -c '.resources.[] | select(.type == "digitalocean_droplet").instances.[]' <"$STAGE/provision/vpn/terraform.tfstate" |
+test -f "$STAGE/provisioning/vpn/terraform.tfstate" && (
+  jq -c '.resources.[] | select(.type == "digitalocean_droplet").instances.[]' <"$STAGE/provisioning/vpn/terraform.tfstate" |
     while read -r DROPLET; do
       IP="$(echo "$DROPLET" | jq -r '.attributes.ipv4_address')"
       REGION="$(echo "$DROPLET" | jq -r '.attributes.region')"
@@ -19,10 +14,10 @@ test -f "$STAGE/provision/vpn/terraform.tfstate" && (
 
       EASYRSA_PKI="$STAGE/secrets/pki/vpn" easyrsa --batch build-server-full "$SERVERNAME" nopass
 
-      scp -i "$STAGE/secrets/ssh/do" "$STAGE/secret/pki/vpn/ca.crt" "$VPS_SUDO_USER@$IP:ca.crt"
-      scp -i "$STAGE/secrets/ssh/do" "$STAGE/secret/pki/vpn/issued/$SERVERNAME.crt" "$VPS_SUDO_USER@$IP:server.crt"
-      scp -i "$STAGE/secrets/ssh/do" "$STAGE/secret/pki/vpn/private/$SERVERNAME.key" "$VPS_SUDO_USER@$IP:server.key"
-      scp -i "$STAGE/secrets/ssh/do" "$STAGE/secret/pki/vpn-users/crl.pem" "$VPS_SUDO_USER@$IP:crl.pem"
+      scp -i "$STAGE/secrets/ssh/do" "$STAGE/secrets/pki/vpn/ca.crt" "$VPS_SUDO_USER@$IP:ca.crt"
+      scp -i "$STAGE/secrets/ssh/do" "$STAGE/secrets/pki/vpn/issued/$SERVERNAME.crt" "$VPS_SUDO_USER@$IP:server.crt"
+      scp -i "$STAGE/secrets/ssh/do" "$STAGE/secrets/pki/vpn/private/$SERVERNAME.key" "$VPS_SUDO_USER@$IP:server.key"
+      scp -i "$STAGE/secrets/ssh/do" "$STAGE/secrets/pki/vpn-users/crl.pem" "$VPS_SUDO_USER@$IP:crl.pem"
       scp -i "$STAGE/secrets/ssh/do" "$STAGE/secrets/ovpn-auth/ovpn_auth_database.yml" "$VPS_SUDO_USER@$IP:ovpn_auth_database.yml"
 
       ssh -i "$STAGE/secrets/ssh/do" -n "$VPS_SUDO_USER@$IP" "sudo bash -c '
