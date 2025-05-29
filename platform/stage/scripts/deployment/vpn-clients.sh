7654f019c5c599c9ad1929627383125f1b8f8f07
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-05-29 20:17:33 +0300

    platform/stage/scripts/deployment/vpn-clients: changes to fasten file uploads, skip server cert generation for consecutive runs, adjust permissions

--- platform/stage/scripts/deployment/vpn-clients.sh
+++ platform/stage/scripts/deployment/vpn-clients.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-PS4='\033[32m$0:$LINENO\033[0m: '
+PS4='\033[34m$0:$LINENO\033[0m: '
 set -xe
 
+: "$(test "$(basename "$PWD")" == stage)"
+: "${STAGE:?}"
 : "${VPS_SUDO_USER:?}"
 
 test -f "$STAGE/provisioning/vpn/terraform.tfstate" && (
@@ -12,24 +14,32 @@ test -f "$STAGE/provisioning/vpn/terraform.tfstate" && (
       REGION="$(echo "$DROPLET" | jq -r '.attributes.region')"
       SERVERNAME="$REGION.do.vpn.logbook"
 
-      EASYRSA_PKI="$STAGE/secrets/pki/vpn" easyrsa --batch build-server-full "$SERVERNAME" nopass
+      if ! test -f "$STAGE/secrets/pki/vpn/issued/$SERVERNAME.crt"; then
+        EASYRSA_PKI="$STAGE/secrets/pki/vpn" easyrsa --batch build-server-full "$SERVERNAME" nopass
+      fi
 
-      scp -i "$STAGE/secrets/ssh/do" "$STAGE/secrets/pki/vpn/ca.crt" "$VPS_SUDO_USER@$IP:ca.crt"
-      scp -i "$STAGE/secrets/ssh/do" "$STAGE/secrets/pki/vpn/issued/$SERVERNAME.crt" "$VPS_SUDO_USER@$IP:server.crt"
-      scp -i "$STAGE/secrets/ssh/do" "$STAGE/secrets/pki/vpn/private/$SERVERNAME.key" "$VPS_SUDO_USER@$IP:server.key"
-      scp -i "$STAGE/secrets/ssh/do" "$STAGE/secrets/pki/vpn-users/crl.pem" "$VPS_SUDO_USER@$IP:crl.pem"
-      scp -i "$STAGE/secrets/ssh/do" "$STAGE/secrets/ovpn-auth/ovpn_auth_database.yml" "$VPS_SUDO_USER@$IP:ovpn_auth_database.yml"
+      scp -i "$STAGE/secrets/ssh/do" \
+        "$STAGE/secrets/pki/vpn/ca.crt" \
+        "$STAGE/secrets/pki/vpn/issued/$SERVERNAME.crt" \
+        "$STAGE/secrets/pki/vpn/private/$SERVERNAME.key" \
+        "$STAGE/secrets/pki/vpn-users/crl.pem" \
+        "$STAGE/secrets/ovpn-auth/ovpn_auth_database.yml" \
+        "$VPS_SUDO_USER@$IP:/home/$VPS_SUDO_USER"
 
-      ssh -i "$STAGE/secrets/ssh/do" -n "$VPS_SUDO_USER@$IP" "sudo bash -c '
+      # shellcheck disable=SC2087
+      ssh -i "$STAGE/secrets/ssh/do" "$VPS_SUDO_USER@$IP" sudo bash <<-HERE
+        PS4='\033[33m$VPS_SUDO_USER@$IP \$LINENO:\033[0m '
         set -xe
         
         cd /home/$VPS_SUDO_USER
-        mv ca.crt crl.pem server.crt server.key ovpn_auth_database.yml /etc/openvpn/
+        mv ca.crt crl.pem ovpn_auth_database.yml /etc/openvpn/
+        mv "$SERVERNAME.crt" /etc/openvpn/server.crt
+        mv "$SERVERNAME.key" /etc/openvpn/server.key
 
         cd /etc/openvpn
         chmod 644 crl.pem
         chmod 744 ovpn_auth_database.yml
-        chown root:root ovpn_auth_database.yml
-      '"
+        chown -R root:root *
+HERE
     done
 )
