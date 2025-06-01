28700da91b866b5fed11d8545914fb957d4a824c
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-06-01 13:40:04 +0300

    platform/stage/source.sh: improves readability

--- platform/stage/source.sh
+++ platform/stage/source.sh
@@ -10,7 +10,6 @@
 export STAGE="${WORKSPACE:?}/platform/stage"
 
 export DO_SSH_FINGERPRINT="$(ssh-keygen -lf "$STAGE/secrets/ssh/do" -E md5 | perl -nE 'say $1 if /(?<=MD5:)([^\s]+)/')"
-export TF_VAR_DO_SSH_FINGERPRINT="$DO_SSH_FINGERPRINT"
 export DO_SSH_KEY_ID="$(doctl compute ssh-key get "$DO_SSH_FINGERPRINT" --output json | jq -r '.[0].id')"
 
 export DO_SSH_PUBKEY="$(cat secrets/ssh/do.pub)"
@@ -23,3 +22,6 @@ export PING_URL="stage.logbook.balaasad.com:8080/api/v1.0.0/ping"
 # env files to declare:
 : "${VPS_SUDO_USER:?}"
 : "${VPS_SUDO_USER_PASSWD_HASH:?}"
+
+# terraform aliases
+export TF_VAR_DO_SSH_FINGERPRINT="$DO_SSH_FINGERPRINT"
