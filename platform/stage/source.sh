9f2a71c2ade04259f9291140d02030f32e525470
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-06-01 13:36:54 +0300

    platform/stage/source.sh: removes unused in-project ssh-config related statements. fixes an ineffective assertion.

--- platform/stage/source.sh
+++ platform/stage/source.sh
@@ -9,38 +9,17 @@
 
 export STAGE="${WORKSPACE:?}/platform/stage"
 
-export DO_SSH_FINGERPRINT="$(
-  ssh-keygen -lf "$STAGE/secrets/ssh/do" -E md5 |
-    perl -nE 'say $1 if /(?<=MD5:)([^\s]+)/'
-)"
+export DO_SSH_FINGERPRINT="$(ssh-keygen -lf "$STAGE/secrets/ssh/do" -E md5 | perl -nE 'say $1 if /(?<=MD5:)([^\s]+)/')"
 export TF_VAR_DO_SSH_FINGERPRINT="$DO_SSH_FINGERPRINT"
-export DO_SSH_KEY_ID="$(
-  doctl compute ssh-key get "$DO_SSH_FINGERPRINT" --output json | jq -r '.[0].id'
-)"
+export DO_SSH_KEY_ID="$(doctl compute ssh-key get "$DO_SSH_FINGERPRINT" --output json | jq -r '.[0].id')"
 
 export DO_SSH_PUBKEY="$(cat secrets/ssh/do.pub)"
 
-alias ssh="ssh -F '$STAGE/artifacts/ssh.conf'"
-alias scp="scp -F '$STAGE/artifacts/ssh.conf'"
-
-_ssh_completion() {
-  local cur opts
-  COMPREPLY=()
-  cur="${COMP_WORDS[COMP_CWORD]}"
-  # prev="${COMP_WORDS[COMP_CWORD - 1]}"
-  opts=$(grep '^Host' "$STAGE/artifacts/ssh.conf" 2>/dev/null | grep -v '[?*]' | cut -d ' ' -f 2-)
-  COMPREPLY=($(compgen -W "$opts" -- "${cur}"))
-  return 0
-}
-complete -F _ssh_completion ssh
-
-# MARK: Utilities
-
-PING_URL="stage.logbook.balaasad.com:8080/api/v1.0.0/ping"
+export PING_URL="stage.logbook.balaasad.com:8080/api/v1.0.0/ping"
 
 # shellcheck disable=SC1091
 . .env
 
 # env files to declare:
 : "${VPS_SUDO_USER:?}"
-: "${VPS_SUDO_USER_PASSWD_HASH}"
+: "${VPS_SUDO_USER_PASSWD_HASH:?}"
