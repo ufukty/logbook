feb0fe7714fda0aeb8f1e214453191876feab30d
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-06-09 22:04:03 +0300

    platform/stage/scripts/secrets: adds missing digitalocen datacenter regions to the list

--- platform/stage/scripts/secrets/reset-vpn-crypt-keys.sh
+++ platform/stage/scripts/secrets/reset-vpn-crypt-keys.sh
@@ -15,6 +15,6 @@ set -xe
 
 mkdir -p "$STAGE/secrets/vpn/tls-crypt"
 
-for REGION in sfo2 sfo3 nyc2 nyc3; do
+for REGION in sfo2 sfo3 tor1 nyc1 nyc3 lon1 ams3 fra1 blr1 sgp1; do
   openvpn --genkey secret "$STAGE/secrets/vpn/tls-crypt/do-$REGION.key"
 done
