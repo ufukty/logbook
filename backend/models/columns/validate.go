bc6f32c96c8e21818ede93aae724fdcd221a6c9a
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-06-15 09:47:27 +0300

    be/models/*/validate.go: removes the `validate` package and its in-package constructors for adopting gohandlers's `validator` package

--- backend/models/columns/validate.go
+++ backend/models/columns/validate.go
@@ -1,8 +1,8 @@
 package columns
 
 import (
-	"logbook/internal/web/validate"
-	"regexp"
+	"github.com/ufukty/gohandlers/pkg/validator"
+	"github.com/ufukty/gohandlers/pkg/validator/validate"
 )
 
 func (v NonNegativeNumber) Validate() error {
@@ -12,34 +12,21 @@ func (v NonNegativeNumber) Validate() error {
 	return nil
 }
 
-type pmm struct {
-	pattern  *regexp.Regexp
-	min, max int
-}
-
-func (b pmm) Validate(v string) error {
-	return validate.StringBasics(v, b.min, b.max, b.pattern)
-}
-
-func r(s string) *regexp.Regexp {
-	return regexp.MustCompile(s)
-}
-
 var (
-	// creditCard   = pmm{r(`^(?:4[0-9]{12}(?:[0-9]{3})?)$`), 13, 19}
-	// htmlDate     = pmm{r(`^\d{4}-\d{2}-\d{2}$`), 6, 8}
-	// htmlDatetime = pmm{r(`^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}$`), 9, 13}
-	// htmlTime     = pmm{r(`^\d{2}:\d{2}$`), 3, 5}
-	// numeric      = pmm{r(`^[1-9][0-9]*$`), 0, 100}
-	// text         = pmm{r(`^[\p{L}0-9 ,.?!'’“”-]+$`), 0, 10000}
-	// url          = pmm{r(`^[\p{L}0-9._%+-]+@[\p{L}0-9.-]+\.[\p{L}]{2,}$`), 0, 10000}
-	email        = pmm{r(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`), 6, 150}
-	groupName    = pmm{r(`[\p{L} ]+`), 2, 100}
-	humanName    = pmm{r(`^\p{L}+([ '-]\p{L}+)*$`), 6, 100}
-	phoneNumber  = pmm{r(`^\+?(\d{1,3})?[ -]?(\d{3})[ -]?(\d{3})[ -]?(\d{4})$`), 10, 15}
-	sessionToken = pmm{r(`[A-Za-z0-9-_]+$`), 256, 256} // pattern is as defined in std lib base64.URLEncoding
-	username     = pmm{r(`^[a-zA-Z]+[a-zA-Z0-9\_\.\-]*$`), 3, 50}
-	uuid_        = pmm{r(`^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$`), len("00000000-0000-0000-0000-000000000000"), len("00000000-0000-0000-0000-000000000000")}
+	// creditCard   = validator.ForStrings(`^(?:4[0-9]{12}(?:[0-9]{3})?)$`, 13, 19)
+	// htmlDate     = validator.ForStrings(`^\d{4}-\d{2}-\d{2}$`, 6, 8)
+	// htmlDatetime = validator.ForStrings(`^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}$`, 9, 13)
+	// htmlTime     = validator.ForStrings(`^\d{2}:\d{2}$`, 3, 5)
+	// numeric      = validator.ForStrings(`^[1-9][0-9]*$`, 0, 100)
+	// text         = validator.ForStrings(`^[\p{L}0-9 ,.?!'’“”-]+$`, 0, 10000)
+	// url          = validator.ForStrings(`^[\p{L}0-9._%+-]+@[\p{L}0-9.-]+\.[\p{L}]{2,}$`, 0, 10000)
+	email        = validator.ForStrings(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`, 6, 150)
+	groupName    = validator.ForStrings(`[\p{L} ]+`, 2, 100)
+	humanName    = validator.ForStrings(`^\p{L}+([ '-]\p{L}+)*$`, 6, 100)
+	phoneNumber  = validator.ForStrings(`^\+?(\d{1,3})?[ -]?(\d{3})[ -]?(\d{3})[ -]?(\d{4})$`, 10, 15)
+	sessionToken = validator.ForStrings(`[A-Za-z0-9-_]+$`, 256, 256) // pattern is as defined in std lib base64.URLEncoding
+	username     = validator.ForStrings(`^[a-zA-Z]+[a-zA-Z0-9\_\.\-]*$`, 3, 50)
+	uuid_        = validator.ForStrings(`^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$`, len("00000000-0000-0000-0000-000000000000"), len("00000000-0000-0000-0000-000000000000"))
 )
 
 func (v AccessId) Validate() error          { return uuid_.Validate(string(v)) }
