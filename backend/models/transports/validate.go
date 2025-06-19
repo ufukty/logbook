f1fbf5aedd2d8f1d5254f60a335c26f217c0b4b2
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-06-19 12:58:54 +0300

    be/models: updates the return type of field validators to match the `gohandlers/pkg/validator/validate` change in v0.31.0

--- backend/models/transports/validate.go
+++ backend/models/transports/validate.go
@@ -6,26 +6,26 @@ import (
 	"github.com/ufukty/gohandlers/pkg/validator/validate"
 )
 
-func (a AntiCsrfToken) Validate() error {
+func (a AntiCsrfToken) Validate() any {
 	return validate.String(string(a), length_anti_csrf_token, length_anti_csrf_token, regexp_base64_url)
 }
 
-func (hb HumanBirthday) Validate() error {
+func (hb HumanBirthday) Validate() any {
 	return validate.Time(time.Time(hb), min_human_birthday, max_human_birthday)
 }
 
-func (v Password) Validate() error {
+func (v Password) Validate() any {
 	return validate.String(string(v), min_length_password, max_length_password, nil)
 }
 
-func (v PhoneGrant) Validate() error {
+func (v PhoneGrant) Validate() any {
 	return validate.String(string(v), length_uuid, length_uuid, regexp_uuid)
 }
 
-func (v EmailGrant) Validate() error {
+func (v EmailGrant) Validate() any {
 	return validate.String(string(v), length_uuid, length_uuid, regexp_uuid)
 }
 
-func (v PasswordGrant) Validate() error {
+func (v PasswordGrant) Validate() any {
 	return validate.String(string(v), length_uuid, length_uuid, regexp_uuid)
 }
