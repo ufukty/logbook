70576083709108c20b3cc1903e7af127f58e8efe
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-06-20 07:35:17 +0300

    be/models/columns: orders field validators to improve readability

--- backend/models/columns/validate.go
+++ backend/models/columns/validate.go
@@ -7,21 +7,23 @@ func (v CollaborationId) Validate() any   { return uuid.Validate(string(v)) }
 func (v CollaboratorId) Validate() any    { return uuid.Validate(string(v)) }
 func (v ControlAreaId) Validate() any     { return uuid.Validate(string(v)) }
 func (v DelegationId) Validate() any      { return uuid.Validate(string(v)) }
-func (v Email) Validate() any             { return email.Validate(string(v)) }
 func (v GroupId) Validate() any           { return uuid.Validate(string(v)) }
 func (v GroupInviteId) Validate() any     { return uuid.Validate(string(v)) }
 func (v GroupMembershipId) Validate() any { return uuid.Validate(string(v)) }
-func (v GroupName) Validate() any         { return groupName.Validate(string(v)) }
-func (v HumanName) Validate() any         { return humanName.Validate(string(v)) }
 func (v LinkId) Validate() any            { return uuid.Validate(string(v)) }
 func (v LoginId) Validate() any           { return uuid.Validate(string(v)) }
 func (v ObjectiveId) Validate() any       { return uuid.Validate(string(v)) }
 func (v OperationId) Validate() any       { return uuid.Validate(string(v)) }
-func (v Phone) Validate() any             { return phoneNumber.Validate(string(v)) }
 func (v PropertiesId) Validate() any      { return uuid.Validate(string(v)) }
 func (v SessionId) Validate() any         { return uuid.Validate(string(v)) }
-func (v SessionToken) Validate() any      { return sessionToken.Validate(string(v)) }
 func (v TagId) Validate() any             { return uuid.Validate(string(v)) }
 func (v UserId) Validate() any            { return uuid.Validate(string(v)) }
-func (v Username) Validate() any          { return username.Validate(string(v)) }
 func (v VersionId) Validate() any         { return uuid.Validate(string(v)) }
+
+func (v Email) Validate() any        { return email.Validate(string(v)) }
+func (v GroupName) Validate() any    { return groupName.Validate(string(v)) }
+func (v HumanName) Validate() any    { return humanName.Validate(string(v)) }
+func (v Phone) Validate() any        { return phoneNumber.Validate(string(v)) }
+func (v SessionToken) Validate() any { return sessionToken.Validate(string(v)) }
+func (v Username) Validate() any     { return username.Validate(string(v)) }
+
