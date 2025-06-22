248d6e0d0e1605b2a3004f83e13f26cd3b4a2591
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-06-22 10:56:10 +0300

    be/models/validators: moves the validators on semantic types from `columns` package for reuse

--- backend/models/columns/validate.go
+++ backend/models/columns/validate.go
@@ -1,35 +1,36 @@
 package columns
 
 import (
+	"logbook/models/validators"
 	"slices"
 )
 
-func (v AccessId) Validate() any          { return uuid.Validate(string(v)) }
-func (v BookmarkId) Validate() any        { return uuid.Validate(string(v)) }
-func (v BottomUpPropsId) Validate() any   { return uuid.Validate(string(v)) }
-func (v CollaborationId) Validate() any   { return uuid.Validate(string(v)) }
-func (v CollaboratorId) Validate() any    { return uuid.Validate(string(v)) }
-func (v ControlAreaId) Validate() any     { return uuid.Validate(string(v)) }
-func (v DelegationId) Validate() any      { return uuid.Validate(string(v)) }
-func (v GroupId) Validate() any           { return uuid.Validate(string(v)) }
-func (v GroupInviteId) Validate() any     { return uuid.Validate(string(v)) }
-func (v GroupMembershipId) Validate() any { return uuid.Validate(string(v)) }
-func (v LinkId) Validate() any            { return uuid.Validate(string(v)) }
-func (v LoginId) Validate() any           { return uuid.Validate(string(v)) }
-func (v ObjectiveId) Validate() any       { return uuid.Validate(string(v)) }
-func (v OperationId) Validate() any       { return uuid.Validate(string(v)) }
-func (v PropertiesId) Validate() any      { return uuid.Validate(string(v)) }
-func (v SessionId) Validate() any         { return uuid.Validate(string(v)) }
-func (v TagId) Validate() any             { return uuid.Validate(string(v)) }
-func (v UserId) Validate() any            { return uuid.Validate(string(v)) }
-func (v VersionId) Validate() any         { return uuid.Validate(string(v)) }
+func (v AccessId) Validate() any          { return validators.Uuid.Validate(string(v)) }
+func (v BookmarkId) Validate() any        { return validators.Uuid.Validate(string(v)) }
+func (v BottomUpPropsId) Validate() any   { return validators.Uuid.Validate(string(v)) }
+func (v CollaborationId) Validate() any   { return validators.Uuid.Validate(string(v)) }
+func (v CollaboratorId) Validate() any    { return validators.Uuid.Validate(string(v)) }
+func (v ControlAreaId) Validate() any     { return validators.Uuid.Validate(string(v)) }
+func (v DelegationId) Validate() any      { return validators.Uuid.Validate(string(v)) }
+func (v GroupId) Validate() any           { return validators.Uuid.Validate(string(v)) }
+func (v GroupInviteId) Validate() any     { return validators.Uuid.Validate(string(v)) }
+func (v GroupMembershipId) Validate() any { return validators.Uuid.Validate(string(v)) }
+func (v LinkId) Validate() any            { return validators.Uuid.Validate(string(v)) }
+func (v LoginId) Validate() any           { return validators.Uuid.Validate(string(v)) }
+func (v ObjectiveId) Validate() any       { return validators.Uuid.Validate(string(v)) }
+func (v OperationId) Validate() any       { return validators.Uuid.Validate(string(v)) }
+func (v PropertiesId) Validate() any      { return validators.Uuid.Validate(string(v)) }
+func (v SessionId) Validate() any         { return validators.Uuid.Validate(string(v)) }
+func (v TagId) Validate() any             { return validators.Uuid.Validate(string(v)) }
+func (v UserId) Validate() any            { return validators.Uuid.Validate(string(v)) }
+func (v VersionId) Validate() any         { return validators.Uuid.Validate(string(v)) }
 
-func (v Email) Validate() any        { return email.Validate(string(v)) }
-func (v GroupName) Validate() any    { return groupName.Validate(string(v)) }
-func (v HumanName) Validate() any    { return humanName.Validate(string(v)) }
-func (v Phone) Validate() any        { return phoneNumber.Validate(string(v)) }
-func (v SessionToken) Validate() any { return sessionToken.Validate(string(v)) }
-func (v Username) Validate() any     { return username.Validate(string(v)) }
+func (v Email) Validate() any        { return validators.Email.Validate(string(v)) }
+func (v GroupName) Validate() any    { return validators.GroupName.Validate(string(v)) }
+func (v HumanName) Validate() any    { return validators.HumanName.Validate(string(v)) }
+func (v Phone) Validate() any        { return validators.PhoneNumber.Validate(string(v)) }
+func (v SessionToken) Validate() any { return validators.SessionToken.Validate(string(v)) }
+func (v Username) Validate() any     { return validators.Username.Validate(string(v)) }
 
 func (v ObjectiveContent) Validate() any { return nil }
 func (v TagTitle) Validate() any         { return nil }
---
+++ backend/models/validators/validator.go
@@ -0,0 +1,20 @@
+package validators
+
+import "github.com/ufukty/gohandlers/pkg/validator"
+
+var (
+	// CreditCard   = validator.ForStrings(`^(?:4[0-9]{12}(?:[0-9]{3})?)$`, 13, 19)
+	// HtmlDate     = validator.ForStrings(`^\d{4}-\d{2}-\d{2}$`, 6, 8)
+	// HtmlDatetime = validator.ForStrings(`^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}$`, 9, 13)
+	// HtmlTime     = validator.ForStrings(`^\d{2}:\d{2}$`, 3, 5)
+	// Numeric      = validator.ForStrings(`^[1-9][0-9]*$`, 0, 100)
+	// Text         = validator.ForStrings(`^[\p{L}0-9 ,.?!'’“”-]+$`, 0, 10000)
+	// Url          = validator.ForStrings(`^[\p{L}0-9._%+-]+@[\p{L}0-9.-]+\.[\p{L}]{2,}$`, 0, 10000)
+	Email        = validator.ForStrings(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`, 6, 150)
+	GroupName    = validator.ForStrings(`[\p{L} ]+`, 2, 100)
+	HumanName    = validator.ForStrings(`^\p{L}+([ '-]\p{L}+)*$`, 6, 100)
+	PhoneNumber  = validator.ForStrings(`^\+?(\d{1,3})?[ -]?(\d{3})[ -]?(\d{3})[ -]?(\d{4})$`, 10, 15)
+	SessionToken = validator.ForStrings(`[A-Za-z0-9-_]+$`, 256, 256) // pattern is as defined in std lib base64.URLEncoding
+	Username     = validator.ForStrings(`^[a-zA-Z]+[a-zA-Z0-9\_\.\-]*$`, 3, 50)
+	Uuid         = validator.ForStrings(`^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$`, len("00000000-0000-0000-0000-000000000000"), len("00000000-0000-0000-0000-000000000000"))
+)
