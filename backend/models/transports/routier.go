94655779ab5690d3ee4884b89eed21cf4b3dc5a3
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-06-22 13:04:28 +0300

    be/models/transports: implements the `Routier` on `Country`, `InviteResponse` and `MemberType`

--- backend/models/transports/routier.go
+++ backend/models/transports/routier.go
@@ -1,10 +1,11 @@
 package transports
 
-func (a *PolicyAction) FromRoute(src string) error {
-	*a = PolicyAction(src)
-	return nil
-}
+func (a *Country) FromRoute(src string) error        { *a = Country(src); return nil }
+func (a *InviteResponse) FromRoute(src string) error { *a = InviteResponse(src); return nil }
+func (a *MemberType) FromRoute(src string) error     { *a = MemberType(src); return nil }
+func (a *PolicyAction) FromRoute(src string) error   { *a = PolicyAction(src); return nil }
 
-func (a *PolicyAction) ToRoute() (string, error) {
-	return string(*a), nil
-}
+func (a *Country) ToRoute() (string, error)        { return string(*a), nil }
+func (a *InviteResponse) ToRoute() (string, error) { return string(*a), nil }
+func (a *MemberType) ToRoute() (string, error)     { return string(*a), nil }
+func (a *PolicyAction) ToRoute() (string, error)   { return string(*a), nil }
