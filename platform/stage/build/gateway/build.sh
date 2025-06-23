ed4b960fc8f6669010df4df591f1119d2502a1ee
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-06-23 08:09:44 +0300

    platform/stage/build/gateway: fixes the `build.sh` missing disable directive for intended client side templating

--- platform/stage/build/gateway/build.sh
+++ platform/stage/build/gateway/build.sh
@@ -56,6 +56,7 @@ trap cleanup EXIT
 ping -o "${IP:?}" && until ssh "${VPS_SUDO_USER:?}@${IP:?}" exit; do sleep 5; done # wait
 
 rsync --verbose --recursive -e ssh "./upload" "${VPS_SUDO_USER:?}@${IP:?}:${VPS_HOME:?}/"
+# shellcheck disable=SC2087
 ssh "${VPS_SUDO_USER:?}@$IP" bash <<EOF
     set -e
     set -v
