14923b55aee9b413e26db04bd6cc9e11dbc82453
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-08-16 08:27:58 +0300

    be/int/web/receptions: merges files for custom response writer implementation

--- backend/internal/web/reception/receptionist.go
+++ backend/internal/web/reception/receptionist.go
@@ -18,6 +18,16 @@ import (
 	"time"
 )
 
+type response struct {
+	http.ResponseWriter
+	Status int
+}
+
+func (r *response) WriteHeader(statusCode int) {
+	r.Status = statusCode
+	r.ResponseWriter.WriteHeader(statusCode)
+}
+
 type RequestId string
 
 type receptionist struct {
