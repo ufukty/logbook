847620950806f8e9449bcedf963158166c85ef43
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-08-22 17:57:12 +0300

    be/int/web/reception/browser: inits package with `Matcher`. It allows user to define all method/header permissions for origin/path via flexible set of rules supports regex matches

---
+++ backend/internal/web/reception/browser/matcher.go
@@ -0,0 +1,84 @@
+package browser
+
+import (
+	"strings"
+)
+
+type allowance struct {
+	methods map[string]any
+	headers map[string]any
+}
+
+func lookup(ss []string) map[string]any {
+	us := make(map[string]any, len(ss))
+	for _, s := range ss {
+		us[strings.ToLower(s)] = nil
+	}
+	return us
+}
+
+func has(m map[string]any, k string) bool {
+	_, ok := m[k]
+	return ok
+}
+
+func contains(asked string, allowed map[string]any) bool {
+	return has(allowed, strings.ToLower(asked))
+}
+
+func containsAll(asked []string, allowed map[string]any) bool {
+	for _, a := range asked {
+		if !contains(a, allowed) {
+			return false
+		}
+	}
+	return true
+}
+
+type StringMatcher interface {
+	MatchString(s string) bool
+}
+
+// Matcher uses custom matcher for origin and path;
+// lowercase character matching for methods and headers
+type Matcher struct {
+	origin    StringMatcher
+	path      StringMatcher
+	allowance *allowance
+}
+
+func NewMatcher(origin, path StringMatcher, allowedmethods, allowedheaders []string) *Matcher {
+	return &Matcher{
+		origin: origin,
+		path:   path,
+		allowance: &allowance{
+			methods: lookup(allowedmethods),
+			headers: lookup(allowedheaders),
+		},
+	}
+}
+
+func (m Matcher) Match(origin, method, path string, headers []string) *allowance {
+	if origin == "" || path == "" || method == "" {
+		return nil
+	}
+	if !m.origin.MatchString(origin) || !m.path.MatchString(path) {
+		return nil
+	}
+	if !contains(method, m.allowance.methods) {
+		return nil
+	}
+	if !containsAll(headers, m.allowance.headers) {
+		return nil
+	}
+	return m.allowance
+}
+
+func matchAny(ms []*Matcher, origin, method, path string, headers []string) *allowance {
+	for _, m := range ms {
+		if a := m.Match(origin, method, path, headers); a != nil {
+			return a
+		}
+	}
+	return nil
+}
