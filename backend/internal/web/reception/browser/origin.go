03cb1264a4fb6d2bbade93f7aa562d4173cdd709
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-08-25 10:28:28 +0300

    be/int/web/reception/browser: implements experimental `OriginChecking` which authorizes the origin for sending a request to an endpoint with a method and set of headers.

---
+++ backend/internal/web/reception/browser/origin.go
@@ -0,0 +1,70 @@
+package browser
+
+import (
+	"maps"
+	"net/http"
+	"slices"
+	"strings"
+)
+
+const (
+	origin                        = "Origin"
+	accessControlAllowCredentials = "Access-Control-Allow-Credentials"
+	accessControlAllowMethods     = "Access-Control-Allow-Methods"
+	accessControlAllowOrigin      = "Access-Control-Allow-Origin"
+	accessControlRequestMethod    = "Access-Control-Request-Method"
+)
+
+// OriginChecking performs (path, method, headers) permission checking
+// both for the cross/same origin and preflight/actual requests
+type OriginChecking struct {
+	Allowances []*Matcher
+}
+
+func (c OriginChecking) preflight(w http.ResponseWriter, r *http.Request) {
+	allowance := matchAny(c.Allowances,
+		r.Header.Get(origin),
+		r.Header.Get(accessControlRequestMethod),
+		r.URL.Path,
+		strings.Split(strings.ReplaceAll(r.Header.Get(accessControlRequestMethod), " ", ""), ","),
+	)
+
+	if allowance == nil {
+		http.Error(w, "<!-- Dear browser, please don't proceed to actually sending the request with the pair of asked method and headers for this origin and path. No worries, otherwise is still safe; it will just be ignored. Thanks for consulting. Beep boop. -->", http.StatusForbidden)
+		return
+	}
+
+	w.Header().Set(accessControlAllowCredentials, "true")
+	w.Header().Set(accessControlAllowMethods, strings.Join(allowance.Methods, ", "))
+	w.Header().Set(accessControlAllowOrigin, "*")
+	w.WriteHeader(http.StatusOK)
+}
+
+func (c OriginChecking) actual(w http.ResponseWriter, r *http.Request) {
+	allowance := matchAny(c.Allowances, r.Header.Get(origin), r.Method, r.URL.Path, slices.Collect(maps.Keys(r.Header)))
+
+	if allowance != nil {
+		http.Error(w, "Please try again using the official website.", http.StatusForbidden)
+		return
+	}
+
+	w.Header().Set(accessControlAllowOrigin, "*")
+	w.WriteHeader(http.StatusOK)
+}
+
+func isPreflight(r *http.Request) bool {
+	return r.Method == http.MethodOptions && has(r.Header, accessControlAllowCredentials)
+}
+
+func (c OriginChecking) Handler(w http.ResponseWriter, r *http.Request) {
+	if o := r.Header.Get(origin); o == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if isPreflight(r) {
+		c.preflight(w, r)
+	} else {
+		c.actual(w, r)
+	}
+}
