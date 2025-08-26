e23fcca8f6717f980130ea6a80935c7eca052c99
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-08-26 18:11:41 +0300

    be/int/web/reception/browser: moves header const declarations into separate sub package `headers`

--- backend/internal/web/reception/browser/origin.go
+++ backend/internal/web/reception/browser/origin.go
@@ -1,18 +1,11 @@
 package browser
 
 import (
+	"logbook/internal/web/reception/browser/headers"
 	"maps"
 	"net/http"
 	"slices"
 	"strings"
-)
-
-const (
-	origin                        = "Origin"
-	accessControlAllowCredentials = "Access-Control-Allow-Credentials"
-	accessControlAllowMethods     = "Access-Control-Allow-Methods"
-	accessControlAllowOrigin      = "Access-Control-Allow-Origin"
-	accessControlRequestMethod    = "Access-Control-Request-Method"
 )
 
 // OriginChecker performs (path, method, headers) permission checking
@@ -23,10 +16,10 @@ type OriginChecker struct {
 
 func (c OriginChecker) preflight(w http.ResponseWriter, r *http.Request) {
 	allowance := matchAny(c.Allowances,
-		r.Header.Get(origin),
-		r.Header.Get(accessControlRequestMethod),
+		r.Header.Get(headers.Origin),
+		r.Header.Get(headers.AccessControlRequestMethod),
 		r.URL.Path,
-		strings.Split(strings.ReplaceAll(r.Header.Get(accessControlRequestMethod), " ", ""), ","),
+		strings.Split(strings.ReplaceAll(r.Header.Get(headers.AccessControlRequestMethod), " ", ""), ","),
 	)
 
 	if allowance == nil {
@@ -34,30 +27,30 @@ func (c OriginChecker) preflight(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Header().Set(accessControlAllowCredentials, "true")
-	w.Header().Set(accessControlAllowMethods, strings.Join(allowance.Methods, ", "))
-	w.Header().Set(accessControlAllowOrigin, "*")
+	w.Header().Set(headers.AccessControlAllowCredentials, "true")
+	w.Header().Set(headers.AccessControlAllowMethods, strings.Join(allowance.Methods, ", "))
+	w.Header().Set(headers.AccessControlAllowOrigin, "*")
 	w.WriteHeader(http.StatusOK)
 }
 
 func (c OriginChecker) actual(w http.ResponseWriter, r *http.Request) {
-	allowance := matchAny(c.Allowances, r.Header.Get(origin), r.Method, r.URL.Path, slices.Collect(maps.Keys(r.Header)))
+	allowance := matchAny(c.Allowances, r.Header.Get(headers.Origin), r.Method, r.URL.Path, slices.Collect(maps.Keys(r.Header)))
 
 	if allowance != nil {
 		http.Error(w, "Please try again using the official website.", http.StatusForbidden)
 		return
 	}
 
-	w.Header().Set(accessControlAllowOrigin, "*")
+	w.Header().Set(headers.AccessControlAllowOrigin, "*")
 	w.WriteHeader(http.StatusOK)
 }
 
 func isPreflight(r *http.Request) bool {
-	return r.Method == http.MethodOptions && has(r.Header, accessControlAllowCredentials)
+	return r.Method == http.MethodOptions && has(r.Header, headers.AccessControlAllowCredentials)
 }
 
 func (c OriginChecker) Handler(w http.ResponseWriter, r *http.Request) {
-	if o := r.Header.Get(origin); o == "" {
+	if o := r.Header.Get(headers.Origin); o == "" {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
