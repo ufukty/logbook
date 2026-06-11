a29137e89030efb3f9f99f52efc47fc75fb30ddf
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2026-06-11 11:30:23 +0300

    be/int/web/reception/summarizer: removes redundant unit printing in string representation.

--- backend/internal/web/reception/summarizer/summarizer.go
+++ backend/internal/web/reception/summarizer/summarizer.go
@@ -63,7 +63,7 @@ func (s Summarizer) Pre(r *http.Request) string {
 }
 
 func (s Summarizer) Post(crw *captured.ResponseWriter, start time.Time) string {
-	return fmt.Sprintf("%s %s %s bytes",
+	return fmt.Sprintf("%s %s %s",
 		s.c.Magenta(crw.StatusRepresentation()),
 		s.c.Green(time.Since(start)),
 		s.c.Cyan(crw.SizeRepresentation()),
